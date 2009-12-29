5639dc2474489d97c3e708b3b9f0164bd4bf15dc
Richard Holland <richard@uwc.net>
2009-12-29 11:02:19 +0000

    Deepak's whitespace in Genbank fix.
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@7285 7c6358e6-4a41-0410-a743-a5b2a554c398

--- core/src/main/java/org/biojavax/bio/seq/io/GenbankFormat.java
+++ core/src/main/java/org/biojavax/bio/seq/io/GenbankFormat.java
@@ -83,6 +83,7 @@ import org.biojavax.utils.StringTools;
  * @author David Scott
  * @author Bubba Puryear
  * @author George Waldon
+ * @author Deepak Sheoran
  * @since 1.5
  */
 public class GenbankFormat extends RichSequenceFormat.HeaderlessFormat {
@@ -570,6 +571,11 @@ public class GenbankFormat extends RichSequenceFormat.HeaderlessFormat {
                 br.mark(320);
                 line = br.readLine();
                 String firstSecKey = section.size() == 0 ? "" : ((String[])section.get(0))[0];
+                if (line != null && line.matches("\\p{Space}*")) {
+		   // regular expression \p{Space}* will match line 
+                   // having only white space characters
+                   continue;
+                }
                 if (line==null || (!line.startsWith(" ") && linecount++>0 && ( !firstSecKey.equals(START_SEQUENCE_TAG)  || line.startsWith(END_SEQUENCE_TAG)))) {
                     // dump out last part of section
                     section.add(new String[]{currKey,currVal.toString()});
