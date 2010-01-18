d49552613ef96495564f95dc2a289b40a1dea095
Michael Heuer <heuermh@acm.org>
2010-01-18 07:09:45 +0000

    merging fastq support from release-1_7_1-branch to trunk, test/java directory
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@7292 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ sequencing/src/test/java/org/biojava/bio/program/fastq/SolexaFastqWriterTest.java
@@ -0,0 +1,69 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
+package org.biojava.bio.program.fastq;
+
+import java.io.IOException;
+
+/**
+ * Unit test for SolexaFastqWriter.
+ */
+public final class SolexaFastqWriterTest
+    extends AbstractFastqWriterTest
+{
+
+    /** {@inheritDoc} */
+    protected FastqWriter createFastqWriter()
+    {
+        return new SolexaFastqWriter();
+    }
+
+    /** {@inheritDoc} */
+    protected Fastq createFastq()
+    {
+        return new FastqBuilder()
+            .withDescription("description")
+            .withSequence("sequence")
+            .withQuality("quality_")
+            .withVariant(FastqVariant.FASTQ_SOLEXA)
+            .build();
+    }
+
+    public void testValidateNotSolexaVariant()
+    {
+        SolexaFastqWriter writer = new SolexaFastqWriter();
+        Appendable appendable = new StringBuilder();
+        Fastq invalid = new FastqBuilder()
+            .withDescription("description")
+            .withSequence("sequence")
+            .withQuality("quality_")
+            .withVariant(FastqVariant.FASTQ_ILLUMINA)
+            .build();
+        try
+        {
+            writer.append(appendable, invalid);
+            fail("validate not fastq-solexa variant expected IOException");
+        }
+        catch (IOException e)
+        {
+            // expected
+        }
+    }
+}
