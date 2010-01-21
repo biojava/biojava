9aa453e7b03e290a63b92f524cfbe5740115055e
Andreas Prlic <andreas.prlic@gmail.com>
2010-01-21 10:30:34 +0000

    new biojava3-core module
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@7345 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ biojava3-core/src/main/java/org/biojava3/core/App.java
@@ -0,0 +1,13 @@
+package org.biojava3.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
