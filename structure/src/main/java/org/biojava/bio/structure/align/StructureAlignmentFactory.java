c0c7be6c51cf190b0b135fee994a705c4ac2aa62
Andreas Prlic <andreas.prlic@gmail.com>
2010-01-27 01:25:42 +0000

    adding support for loading jFatCat, if it is in the classPath
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@7411 7c6358e6-4a41-0410-a743-a5b2a554c398

--- structure/src/main/java/org/biojava/bio/structure/align/StructureAlignmentFactory.java
+++ structure/src/main/java/org/biojava/bio/structure/align/StructureAlignmentFactory.java
@@ -13,33 +13,67 @@ public class StructureAlignmentFactory {
 
 	public static StructureAlignment[] getAllAlgorithms(){
 		StructureAlignment[] algorithms = new StructureAlignment[3];
-		
+
 		algorithms[0] = new CeMain();
 		algorithms[1] = new CeSideChainMain();		
 		algorithms[2] = new SmithWaterman3Daligner();
-		
+
 		//algorithms[3] = new BioJavaStructureAlignment();
 		return algorithms;
-		
+
 	}
-	
+
 	public static StructureAlignment getAlgorithm(String name) throws StructureException{
 		StructureAlignment[] algorithms = getAllAlgorithms();
 		for ( StructureAlignment algo : algorithms){
 			if (algo.getAlgorithmName().equalsIgnoreCase(name))
 				return algo;
 		}
+
+		// check if the fatcat source code is in the class path
+		// we still need to seek permission to get jFatCat released under the LGPL
+		if  ( name.equals("jFatCat_rigid")){
+			return getFatCatRigid();
+		}
+		
+		if ( name.equals("jFatCat_flexible")){
+			return getFatCatFlexible();
+		}
 		throw new StructureException("Unknown alignment algorithm: " + name);
+	}
+
+	private static StructureAlignment getFatCatRigid() throws StructureException{
+
+		String className = "org.rcsb.fatcat.FatCatRigid";
+		try {
+			Class c = Class.forName(className);
+			Object o = c.newInstance();
+			return (StructureAlignment)o;
+		} catch (Exception e){
+			throw new StructureException("Could not find FatCatRigid in the classpath.");
+		}
+	}
+	
+	private static StructureAlignment getFatCatFlexible() throws StructureException{
+
+		String className = "org.rcsb.fatcat.FatCatFlexible";
+		try {
+			Class c = Class.forName(className);
+			Object o = c.newInstance();
+			return (StructureAlignment)o;
+		} catch (Exception e){
+			throw new StructureException("Could not find FatCatFlexible in the classpath.");
+		}
 	}
 	
 	public static String[] getAllAlgorithmNames(){
 		StructureAlignment[] algos = getAllAlgorithms();
 		List<String> names = new ArrayList<String>();
-		
+
 		for (StructureAlignment alg : algos){
 			names.add(alg.getAlgorithmName());
 		}
-		
+
 		return (String[])names.toArray(new String[names.size()]);
 	}
 }
