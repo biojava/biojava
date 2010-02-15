430cf1ff718f5f17819d183ad20a04471bff6e5e
Andy Yates <ayates@ebi.ac.uk>
2010-02-15 23:51:12 +0000

    Adding the ability to map between ambiguity symbols easily and these are some tests
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@7489 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ biojava3-core/src/test/java/org/biojava3/core/sequence/compound/AmbiguityDNACompoundTests.java
@@ -0,0 +1,33 @@
+package org.biojava3.core.sequence.compound;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class AmbiguityDNACompoundTests {
+
+  private AmbiguityDNACompoundSet set = AmbiguityDNACompoundSet.getDNACompoundSet();
+
+  @Test
+  public void testAmbiguity() {
+    NucleotideCompound actual = set.getAmbiguity(getCompounds("M","V"));
+    assertEquals("Checking M & G = V", getCompounds("V")[0], actual);
+  }
+
+  @Test
+  public void testBasicAmbiguity() {
+    NucleotideCompound actual = set.getAmbiguity(getCompounds("A","C"));
+    assertEquals("Checking A & C = M", getCompounds("M")[0], actual);
+  }
+
+  private NucleotideCompound[] getCompounds(String... compoundStrings) {
+    List<NucleotideCompound> c = new ArrayList<NucleotideCompound>();
+    for(String s: compoundStrings) {
+      c.add(set.getCompoundForString(s));
+    }
+    return c.toArray(new NucleotideCompound[0]);
+  }
+}
