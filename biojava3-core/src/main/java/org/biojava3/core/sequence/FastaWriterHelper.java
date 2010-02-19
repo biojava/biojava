b41fbeaa2cbb1ae972117c7ed921bb1a81e46c26
Scooter Willis <willishf@gmail.com>
2010-02-19 22:17:33 +0000

    Added FastaReaderHelper and FastaWriterHelper classes
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@7512 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ biojava3-core/src/main/java/org/biojava3/core/sequence/FastaWriterHelper.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.biojava3.core.sequence;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Collection;
+import org.biojava3.core.sequence.io.FastaWriter;
+import org.biojava3.core.sequence.io.GenericFastaHeaderFormat;
+
+/**
+ *
+ * @author Scooter Willis <willishf at gmail dot com>
+ */
+public class FastaWriterHelper {
+
+    /**
+     * Write collection of protein sequences to a file
+     * @param file
+     * @param proteinSequences
+     * @throws Exception
+     */
+
+    public static void writeProteinSequence(File file, Collection<ProteinSequence> proteinSequences) throws Exception {
+        FileOutputStream outputStream = new FileOutputStream(file);
+        writeProteinSequence(outputStream,proteinSequences);
+        outputStream.close();
+    }
+
+    public static void writeProteinSequence(OutputStream outputStream , Collection<ProteinSequence> proteinSequences) throws Exception {
+
+        FastaWriter fastaWriter = new FastaWriter(outputStream, proteinSequences, new GenericFastaHeaderFormat());
+        fastaWriter.process();
+
+    }
+}
