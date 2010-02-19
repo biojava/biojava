293b94247db527cd05bf36f66fc01c3ea1b1788b
Scooter Willis <willishf@gmail.com>
2010-02-19 22:18:47 +0000

    Missing for some reason
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@7513 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-core/src/main/java/org/biojava3/core/sequence/io/util/ClasspathResource.java
+++ biojava3-core/src/main/java/org/biojava3/core/sequence/io/util/ClasspathResource.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
-
+ 
 import org.biojava3.core.exceptions.ParserException;
 
 public class ClasspathResource {
