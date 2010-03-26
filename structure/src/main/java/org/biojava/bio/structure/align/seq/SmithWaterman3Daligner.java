64f384104dc950fe52357523814094386e087efb
Andreas Prlic <andreas.prlic@gmail.com>
2010-03-26 16:18:16 +0000

    update to latest version of alignment module
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@7604 7c6358e6-4a41-0410-a743-a5b2a554c398

--- structure/src/main/java/org/biojava/bio/structure/align/seq/SmithWaterman3Daligner.java
+++ structure/src/main/java/org/biojava/bio/structure/align/seq/SmithWaterman3Daligner.java
@@ -9,7 +9,7 @@ import java.util.List;
 import org.biojava.bio.BioException;
 
 import org.biojava.bio.alignment.AlignmentAlgorithm;
-import org.biojava.bio.alignment.SimpleAlignment;
+import org.biojava.bio.alignment.AlignmentPair;
 import org.biojava.bio.alignment.SmithWaterman;
 import org.biojava.bio.alignment.SubstitutionMatrix;
 
@@ -20,7 +20,6 @@ import org.biojava.bio.structure.Group;
 import org.biojava.bio.structure.StructureException;
 import org.biojava.bio.structure.StructureTools;
 import org.biojava.bio.structure.align.AbstractStructureAlignment;
-import org.biojava.bio.structure.align.StrucAligParameters;
 import org.biojava.bio.structure.align.StructureAlignment;
 import org.biojava.bio.structure.align.ce.CECalculator;
 import org.biojava.bio.structure.align.ce.CeParameters;
@@ -104,14 +103,12 @@ public class SmithWaterman3Daligner extends AbstractStructureAlignment implement
 			Sequence query  = ProteinTools.createProteinSequence(seq1,  "seq1");
 			Sequence target = ProteinTools.createProteinSequence(seq2,  "seq2");
 			
-			aligner.pairwiseAlignment(query, target);
-			// Perform an alignment and save the results.
-			SimpleAlignment alig = (SimpleAlignment) aligner.getAlignment() ;
+			AlignmentPair aligPair = aligner.pairwiseAlignment(query, target);
 
 			// Print the alignment to the screen
 			// System.out.println(aligner.getAlignmentString());
 			// convert to a 3D alignment...
-			 afpChain = convert(aligner,ca1,ca2,alig);
+			 afpChain = convert(aligner,ca1,ca2,aligPair);
 			
 		} catch (Exception e){
 
@@ -120,7 +117,7 @@ public class SmithWaterman3Daligner extends AbstractStructureAlignment implement
 		return afpChain;
 	}
 
-	private AFPChain convert(AlignmentAlgorithm aligner,Atom[] ca1, Atom[] ca2,  SimpleAlignment alig) throws StructureException {
+	private AFPChain convert(AlignmentAlgorithm aligner,Atom[] ca1, Atom[] ca2,  AlignmentPair alig) throws StructureException {
 		AFPChain afpChain = new AFPChain();
 		int ca1Length = ca1.length;
 		int ca2Length = ca2.length;		
@@ -132,7 +129,7 @@ public class SmithWaterman3Daligner extends AbstractStructureAlignment implement
 		int nAtom=0; 
 		int nGaps=0;
 		
-		List<Object> labels = alig.getLabels();
+		List<String> labels = alig.getLabels();
 		
 		if ( ! (labels.size() == 2)){
 			throw new StructureException("Expected labels of length 2 but got " + labels.size());
