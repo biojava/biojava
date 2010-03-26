1c511a03e5c0c026788e37d323731bbdfcd3c0a7
Andreas Prlic <andreas.prlic@gmail.com>
2010-03-26 16:18:06 +0000

    update to latest version of alignment module
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@7603 7c6358e6-4a41-0410-a743-a5b2a554c398

--- structure/src/main/java/org/biojava/bio/structure/io/SeqRes2AtomAligner.java
+++ structure/src/main/java/org/biojava/bio/structure/io/SeqRes2AtomAligner.java
@@ -32,6 +32,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.biojava.bio.BioException;
+import org.biojava.bio.alignment.AlignmentPair;
 import org.biojava.bio.alignment.NeedlemanWunsch;
 import org.biojava.bio.alignment.SimpleAlignment;
 import org.biojava.bio.alignment.SubstitutionMatrix;
@@ -219,7 +220,7 @@ public class SeqRes2AtomAligner {
         //System.out.println("align seq2 " + seq2);
 
 
-        SimpleAlignment simpleAli = null;
+        AlignmentPair aligPair = null;
         try  {
 
 
@@ -235,18 +236,9 @@ public class SeqRes2AtomAligner {
             	System.out.println("seq2: " + bjseq2.seqString());
             }
          
-            org.biojava.bio.alignment.Alignment ali = aligner.getAlignment(bjseq1,bjseq2);
-            if ( ! (ali instanceof SimpleAlignment )) {
-                throw new Exception ("Alignment is not a SimpleAlignment!");
-
-            }
-
-            simpleAli = (SimpleAlignment) ali;
-     
-            alignmentString = aligner.getAlignmentString();
-     
-            if (DEBUG)
-                System.out.println(alignmentString);
+            
+            aligPair= aligner.pairwiseAlignment(bjseq1,bjseq2);
+            
         } catch (Exception e){
             e.printStackTrace();
             System.err.println("align seq1 " + seq1);
@@ -254,13 +246,13 @@ public class SeqRes2AtomAligner {
 
         }
 
-        if ( simpleAli == null)
+        if ( aligPair == null)
             throw new StructureException("could not align objects!");
 
         //System.out.println(ali.getAlphabet());
-        SymbolList lst1 = simpleAli.symbolListForLabel("seq1");
+        SymbolList lst1 = aligPair.symbolListForLabel("seq1");
         //System.out.println("from seqres : " + lst1.seqString());
-        SymbolList lst2 = simpleAli.symbolListForLabel("seq2");
+        SymbolList lst2 = aligPair.symbolListForLabel("seq2");
 
         boolean noMatchFound = mapChains(seqRes,lst1,atomRes,lst2,gapSymbol);
         return noMatchFound;
