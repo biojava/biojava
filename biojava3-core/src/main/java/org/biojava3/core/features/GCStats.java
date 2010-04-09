0158cc4ae267677e3e8cca4def947f02e9073512
Andy Yates <ayates@ebi.ac.uk>
2010-04-09 15:55:00 +0000

    New GC stats using the SequenceUtils class
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@7658 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-core/src/main/java/org/biojava3/core/features/GCStats.java
+++ biojava3-core/src/main/java/org/biojava3/core/features/GCStats.java
@@ -5,7 +5,10 @@
 package org.biojava3.core.features;
 
 import java.util.Collection;
-import org.biojava3.core.sequence.DNASequence;
+
+import org.biojava3.core.sequence.compound.NucleotideCompound;
+import org.biojava3.core.sequence.template.Sequence;
+import org.biojava3.core.sequence.template.SequenceMixin;
 
 /**
  *
@@ -13,37 +16,53 @@ import org.biojava3.core.sequence.DNASequence;
  */
 public class GCStats {
 
-    public static double getGCStats(Collection<DNASequence> sequences) {
-        double gcCount = 0;
-        double total = 0;
-
-        for (DNASequence sequence : sequences) {
-            char[] dna = sequence.toString().toCharArray();
-            for (char d : dna) {
-                if (d == 'G' || d == 'C' || d == 'g' || d == 'c') {
-                    gcCount++;
-                }
-                total++;
-            }
-        }
-
-        return (gcCount / total) * 100.0;
+  /**
+   * Calculates the total percentage of GC found in the given sequences.
+   *
+   * @param sequences List of Sequences to do GC count on
+   * @return The percentage of GC in the Sequences
+   */
+  public static double getGCStats(Collection<Sequence<NucleotideCompound>> sequences) {
+    double gcCount = 0;
+    double total = 0;
+    for (Sequence<NucleotideCompound> sequence : sequences) {
+      gcCount += SequenceMixin.countGC(sequence);
+      total += sequence.getLength();
     }
+    return (gcCount / total) * 100.0;
+  }
 
-    public static double getGCStatsString(Collection<String> sequences) {
-        double gcCount = 0;
-        double total = 0;
-
-        for (String sequence : sequences) {
-            char[] dna = sequence.toCharArray();
-            for (char d : dna) {
-                if (d == 'G' || d == 'C' || d == 'g' || d == 'c') {
-                    gcCount++;
-                }
-                total++;
-            }
+  /**
+   * Calculates the total percentage of GC found in the given sequences.
+   *
+   * @param sequences List of Strings to do GC count on
+   * @return The percentage of GC in the strings
+   */
+  public static double getGCStatsString(Collection<String> sequences) {
+    double gcCount = 0;
+    double total = 0;
+    for (String sequence : sequences) {
+      char[] dna = sequence.toCharArray();
+      for (char d : dna) {
+        if (d == 'G' || d == 'C' || d == 'g' || d == 'c') {
+          gcCount++;
         }
-
-        return (gcCount / total) * 100.0;
+        total++;
+      }
     }
+    return (gcCount / total) * 100.0;
+  }
+
+  /**
+   * Calculates the percentage of GC in the given sequence
+   *
+   * @param sequence The {@link NucleotideCompound} {@link Sequence} to perform
+   * the GC analysis on
+   * @return The percentage of GC in the sequence
+   */
+  public static double getGCStats(Sequence<NucleotideCompound> sequence) {
+    double count = SequenceMixin.countGC(sequence);
+    double size = sequence.getLength();
+    return (count / size) * 100.0;
+  }
 }
