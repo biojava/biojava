1d469c7fbf48460782a6a08b182479ff0a39bf22
Andy Yates <ayates@ebi.ac.uk>
2010-04-09 15:53:40 +0000

    More docs
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@7655 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-core/src/main/java/org/biojava3/core/sequence/views/WindowedSequence.java
+++ biojava3-core/src/main/java/org/biojava3/core/sequence/views/WindowedSequence.java
@@ -7,6 +7,24 @@ import java.util.List;
 import org.biojava3.core.sequence.template.Compound;
 import org.biojava3.core.sequence.template.Sequence;
 
+/**
+ * A sliding window view of a sequence which does not implement any
+ * interfaces like {@link Sequence} because they do not fit how this works.
+ * For each index requested we return a List of compounds back.
+ *
+ * If you perform a view on a Sequence whose length is not a multiple of the
+ * window the final window will be omitted i.e. if we have the sequence AGCGG
+ * and a window of 3 then you will only see AGC since GG exceeds the calculated
+ * length of this sequence.
+ *
+ * Because this does not implement a Sequence interface we do not recommend
+ * passing this class around. If you need to represent a windowed sequence
+ * as a real Sequence then translate it into a new Compound
+ *
+ * @author ayates
+ *
+ * @param <C> The type of compound we return from a window
+ */
 public class WindowedSequence<C extends Compound> implements Iterable<List<C>>{
 
   private final Sequence<C> sequence;
@@ -17,10 +35,16 @@ public class WindowedSequence<C extends Compound> implements Iterable<List<C>>{
     this.windowSize = windowSize;
   }
 
+  /**
+   * Access the current window size
+   */
   public int getWindowSize() {
     return windowSize;
   }
 
+  /**
+   * Access the sequence which backs this window
+   */
   public Sequence<C> getBackingSequence() {
     return sequence;
   }
@@ -33,31 +57,45 @@ public class WindowedSequence<C extends Compound> implements Iterable<List<C>>{
     return ((index - 1) * getWindowSize()) + 1;
   }
 
+  /**
+   * Returns the size of the windowed sequence which is the length by the
+   * window size. Trailing Compounds are omitted.
+   */
   public int getLength() {
     return getBackingSequence().getLength() / getWindowSize();
   }
 
+  /**
+   * For a given position into the windowed view this will return those
+   * compounds we can see in the window. i.e. in the sequence AGGCCT requesting
+   * index 1 returns AGG and requesting index 2 return CCT.
+   *
+   * @param index Windowed index position
+   * @return The List of compounds
+   */
   public List<C> getCompounds(int index) {
     int start = toStartIndex(index);
     int window = getWindowSize();
-    List<C> output = new ArrayList<C>(3);
+    List<C> output = new ArrayList<C>(window);
     for(int i = 0; i < window; i++) {
       output.add(getBackingSequence().getCompoundAt(start+i));
     }
     return output;
   }
 
-
+  /**
+   * Iterator of all List of compounds available in a windowed sequence.
+   */
   public Iterator<List<C>> iterator() {
     final int start = 1;
     final int end = getLength();
     return new Iterator<List<C>>() {
       private int currentIndex = start;
-     
+
       public boolean hasNext() {
         return currentIndex <= end;
       }
-    
+
       public List<C> next() {
         return WindowedSequence.this.getCompounds(currentIndex++);
       }
