24be2f8742c69eac52df1712e3821bdcceadc62f
Andreas Prlic <andreas.prlic@gmail.com>
2010-06-10 19:14:22 +0000

    adding tmp cache for junit tests
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@7944 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ biojava3-protmod/src/test/java/org/biojava3/protmod/TmpAtomCache.java
@@ -0,0 +1,39 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ * Created on May 11, 2010
+ * Author: Andreas Prlic 
+ *
+ */
+
+package org.biojava3.protmod;
+
+import org.biojava.bio.structure.align.util.AtomCache;
+
+public class TmpAtomCache
+{
+   static String tmpDir = System.getProperty("java.io.tmpdir");
+   public static AtomCache cache = new AtomCache(tmpDir,true );
+   static {
+	   cache.setLoadChemCompInfo(true);
+	   cache.setAutoFetch(true);
+	   cache.setAlignSeqRes(true);
+	   
+   }
+}
