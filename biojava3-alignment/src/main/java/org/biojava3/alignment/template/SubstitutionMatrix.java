f65ef503c4a5524869b26bbaf83dbd29c29dea20
Mark Chapman <chapman@cs.wisc.edu>
2010-06-11 07:28:01 +0000

    documentation comments
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@7949 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-alignment/src/main/java/org/biojava3/alignment/template/SubstitutionMatrix.java
+++ biojava3-alignment/src/main/java/org/biojava3/alignment/template/SubstitutionMatrix.java
@@ -26,28 +26,96 @@ package org.biojava3.alignment.template;
 import org.biojava3.core.sequence.template.Compound;
 import org.biojava3.core.sequence.template.CompoundSet;
 
-public interface SubstitutionMatrix<S extends CompoundSet<C>, C extends Compound> {
+/**
+ * Defines a data structure which holds the score (penalty or bonus) given during alignment for the exchange of one
+ * {@link Compound} in a sequence for another.
+ *
+ * @author Mark Chapman
+ * @param <C> each element of the matrix corresponds to a pair of {@link Compound}s of type C
+ */
+public interface SubstitutionMatrix<C extends Compound> {
 
-    S getCompoundSet();
+    /**
+     * Returns the {@link CompoundSet} on which the matrix is defined.
+     *
+     * @return the {@link CompoundSet} on which the matrix is defined
+     */
+    CompoundSet<C> getCompoundSet();
 
+    /**
+     * Returns the description of this matrix.
+     *
+     * @return description
+     */
     String getDescription();
 
+    /**
+     * Returns entire matrix.
+     *
+     * @return matrix
+     */
     short[][] getMatrix();
 
+    /**
+     * Returns this matrix as a formatted String with {@link Compound} labels along the axes.
+     *
+     * @return this matrix as a formatted String
+     */
     String getMatrixAsString();
 
+    /**
+     * Returns the maximum value in this matrix.
+     *
+     * @return the maximum value in this matrix
+     */
     short getMaxValue();
 
+    /**
+     * Returns the minimum value in this matrix.
+     *
+     * @return the minimum value in this matrix
+     */
     short getMinValue();
 
+    /**
+     * Returns the name (short description) of this matrix.
+     *
+     * @return name
+     */
     String getName();
 
+    /**
+     * Returns value in matrix for conversion from first {@link Compound} to the second.  If an argument does not
+     * belong to the {@link CompoundSet}, this could either throw an {@link IllegalArgumentException} or it could
+     * return {@link #getMinValue()}.
+     *
+     * @param from original {@link Compound}
+     * @param to replacement {@link Compound}
+     * @return value in matrix for conversion from first {@link Compound} to the second
+     * @throws IllegalArgumentException possibly, if an argument does not belong to the {@link CompoundSet}
+     */
     short getValue(C from, C to);
 
-    void normalizeMatrix(short scale);
+    /**
+     * Rescales the matrix so that to {@link #getMaxValue()} - {@link #getMinValue()} = scale.
+     *
+     * @param scale new normalization scale of this matrix
+     * @throws IllegalArgumentException if scale < 1
+     */
+    SubstitutionMatrix<C> normalizeMatrix(short scale);
 
+    /**
+     * Sets the description of this matrix.
+     *
+     * @param description new description
+     */
     void setDescription(String description);
 
+    /**
+     * Sets the name (short description) of this matrix.
+     *
+     * @param name new name
+     */
     void setName(String name);
 
 }
