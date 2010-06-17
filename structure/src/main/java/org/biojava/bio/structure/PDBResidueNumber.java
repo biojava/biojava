e91712db8cb061f27074a10c3a9ccbb7a82822f0
Andreas Prlic <andreas.prlic@gmail.com>
2010-06-17 18:43:30 +0000

    new utility method to deal with PDB residue numbering and insertion codes
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@7978 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ structure/src/main/java/org/biojava/bio/structure/PDBResidueNumber.java
@@ -0,0 +1,64 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ * Created on Jun 17, 2010
+ * Author: ap3 
+ *
+ */
+
+package org.biojava.bio.structure;
+
+/** Everything that is needed to uniquely describe a residue position
+ * 
+ * @author Andreas Prlic
+ *
+ */
+public class PDBResidueNumber
+{
+   
+   String chainId;
+   String insCode;
+   Integer residueNumber;
+   
+   public String getChainId()
+   {
+      return chainId;
+   }
+   public void setChainId(String chainId)
+   {
+      this.chainId = chainId;
+   }
+   public String getInsCode()
+   {
+      return insCode;
+   }
+   public void setInsCode(String insCode)
+   {
+      this.insCode = insCode;
+   }
+   public Integer getResidueNumber()
+   {
+      return residueNumber;
+   }
+   public void setResidueNumber(Integer residueNumber)
+   {
+      this.residueNumber = residueNumber;
+   }
+
+}
