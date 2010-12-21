6e46bdf8bf4d512eb330ab316c7c427af632057b
Andreas Prlic <andreas.prlic@gmail.com>
2010-12-21 23:41:42 +0000

    more javadoc
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@8632 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-structure/src/main/java/org/biojava/bio/structure/io/mmcif/ChemCompProvider.java
+++ biojava3-structure/src/main/java/org/biojava/bio/structure/io/mmcif/ChemCompProvider.java
@@ -2,13 +2,18 @@ package org.biojava.bio.structure.io.mmcif;
 
 import org.biojava.bio.structure.io.mmcif.model.ChemComp;
 
-/** Interface that is implememted by all classes that can instantiate ChemComp groups
+/** Interface that is implememted by all classes that can provide {@link ChemComp} definitions.
  * 
  * @author Andreas Prlic
  *
  */
 public interface ChemCompProvider {
 
+	/** Returns a new instance of a chemical component definition.
+	 * 
+	 * @param recordName the ID of the {@link ChemComp}
+	 * @return a new {@link ChemComp} definition.
+	 */
 	ChemComp getChemComp(String recordName) ;
 
 }
