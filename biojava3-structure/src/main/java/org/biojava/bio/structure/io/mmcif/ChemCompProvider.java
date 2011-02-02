4a8d278b4a37f12cb05e52f51ae885d14784a93d
Andreas Prlic <andreas.prlic@gmail.com>
2011-02-02 00:48:09 +0000

    fixing typo
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@8709 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-structure/src/main/java/org/biojava/bio/structure/io/mmcif/ChemCompProvider.java
+++ biojava3-structure/src/main/java/org/biojava/bio/structure/io/mmcif/ChemCompProvider.java
@@ -2,10 +2,10 @@ package org.biojava.bio.structure.io.mmcif;
 
 import org.biojava.bio.structure.io.mmcif.model.ChemComp;
 
-/** Interface that is implememted by all classes that can provide {@link ChemComp} definitions.
+/** Interface that is implemented by all classes that can provide {@link ChemComp} definitions.
  * 
  * @author Andreas Prlic
- *
+ * @since 3.0
  */
 public interface ChemCompProvider {
 
