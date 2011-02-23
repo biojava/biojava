9fa6ec018a89d553d20dac1bb690496ef96ef73b
Andreas Prlic <andreas.prlic@gmail.com>
2011-02-23 02:48:06 +0000

    cleanup
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@8805 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-structure/src/main/java/org/biojava/bio/structure/io/mmcif/AllChemCompProvider.java
+++ biojava3-structure/src/main/java/org/biojava/bio/structure/io/mmcif/AllChemCompProvider.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 
