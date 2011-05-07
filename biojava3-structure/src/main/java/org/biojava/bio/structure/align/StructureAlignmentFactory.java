f4602bae950449a8954f4c04a5136ba9a548d640
Spencer Bliven <sbliven@ucsd.edu>
2011-05-07 00:24:05 +0000

    Added method to StructureAlignmentFactory allowing algorithms to be added at runtime.
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@8916 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-structure/src/main/java/org/biojava/bio/structure/align/StructureAlignmentFactory.java
+++ biojava3-structure/src/main/java/org/biojava/bio/structure/align/StructureAlignmentFactory.java
@@ -1,7 +1,6 @@
 package org.biojava.bio.structure.align;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.biojava.bio.structure.StructureException;
@@ -14,55 +13,70 @@ import org.biojava.bio.structure.align.seq.SmithWaterman3Daligner;
 
 public class StructureAlignmentFactory {
 
-   public static StructureAlignment[] getAllAlgorithms(){
+	private static List<StructureAlignment> algorithms = new ArrayList<StructureAlignment>();
+	
+	static {
+		algorithms.add( new CeMain() );
+		algorithms.add( new CeCPMain() );
+		//algorithms.add(new CeSideChainMain());
 
+		StructureAlignment fatcatRigid    = new FatCatRigid();
+		StructureAlignment fatcatFlexible = new FatCatFlexible();
 
-      List<StructureAlignment> algorithms = new LinkedList<StructureAlignment>();
+		if ( fatcatRigid != null) {
+			algorithms.add( fatcatRigid) ;
 
-      algorithms.add( new CeMain() );
-      algorithms.add( new CeCPMain() );
-      //algorithms.add(new CeSideChainMain());
+		}
+		if ( fatcatFlexible != null){
+			algorithms.add( fatcatFlexible );
 
-      StructureAlignment fatcatRigid    = new FatCatRigid();
-      StructureAlignment fatcatFlexible = new FatCatFlexible();
+		}
+		algorithms.add( new SmithWaterman3Daligner()) ;
+		//algorithms.add( new BioJavaStructureAlignment());
+	}
+	
+	/**
+	 * Adds a new StructureAlignment algorithm to the list.
+	 * 
+	 * Only one instance is stored for each algorithmName, so it is possible
+	 * that a different instance may be returned by getAlgorithm(alg.getAlgorithmName())
+	 * 
+	 * @param alg the alignment algorithm
+	 */
+	public static void addAlgorithm(StructureAlignment alg) {
+		//ensure uniqueness
+		try {
+			getAlgorithm(alg.getAlgorithmName());
+			// algorithm was found. Do nothing.
+		} catch(StructureException e) {
+			// no algorithm found, so it's new
+			algorithms.add(alg);
+		}
+	}
 
-      if ( fatcatRigid != null) {
-         algorithms.add( fatcatRigid) ;
+	public static StructureAlignment getAlgorithm(String name) throws StructureException{
+		StructureAlignment[] algorithms = getAllAlgorithms();
+		for ( StructureAlignment algo : algorithms){
+			if (algo.getAlgorithmName().equalsIgnoreCase(name))
+				return algo;
+		}
 
-      }
-      if ( fatcatFlexible != null){
-         algorithms.add( fatcatFlexible );
+		throw new StructureException("Unknown alignment algorithm: " + name);
+	}
+	
+	public static StructureAlignment[] getAllAlgorithms(){
+		return algorithms.toArray(new StructureAlignment[algorithms.size()]);
+	}
 
-      }
-      algorithms.add( new SmithWaterman3Daligner()) ;
+	public static String[] getAllAlgorithmNames(){
+		StructureAlignment[] algos = getAllAlgorithms();
+		List<String> names = new ArrayList<String>();
 
+		for (StructureAlignment alg : algos){
+			names.add(alg.getAlgorithmName());
+		}
 
-      //algorithms.add( new BioJavaStructureAlignment());
-      return algorithms.toArray(new StructureAlignment[algorithms.size()]);
+		return (String[])names.toArray(new String[names.size()]);
+	}
 
-   }
-
-   public static StructureAlignment getAlgorithm(String name) throws StructureException{
-      StructureAlignment[] algorithms = getAllAlgorithms();
-      for ( StructureAlignment algo : algorithms){
-         if (algo.getAlgorithmName().equalsIgnoreCase(name))
-            return algo;
-      }
-
-
-      throw new StructureException("Unknown alignment algorithm: " + name);
-   }
-
-
-
-   public static String[] getAllAlgorithmNames(){
-      StructureAlignment[] algos = getAllAlgorithms();
-      List<String> names = new ArrayList<String>();
-
-      for (StructureAlignment alg : algos){
-         names.add(alg.getAlgorithmName());
-      }
-
-      return (String[])names.toArray(new String[names.size()]);
-   }
 }
