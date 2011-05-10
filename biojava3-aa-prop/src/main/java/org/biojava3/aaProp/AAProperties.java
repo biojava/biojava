d990d663b19de3640c41f53b0adc22a7e7483312
Peter Troshin <to.petr@gmail.com>
2011-05-10 10:58:57 +0000

    Comments on Ah Fu interface for simple AA physico-chemical properties calculations
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@8927 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-aa-prop/src/main/java/org/biojava3/aaProp/AAProperties.java
+++ biojava3-aa-prop/src/main/java/org/biojava3/aaProp/AAProperties.java
@@ -26,63 +26,106 @@ import java.util.Hashtable;
 
 import org.biojava3.core.sequence.ProteinSequence;
 
-
 /**
- * An interface to generate some basic physico-chemical properties of protein sequences
+ * An interface to generate some basic physico-chemical properties of protein
+ * sequences
+ * 
+ * TODO: Keep the capitalization consistent (use Index instead of index)
+ * 
+ * TODO The javadoc for each method should contain a short description of the
+ * method. The comments should make clear what the method is doing, for example
+ * what is the instability index? Is there only one way to calculate it? If so
+ * it should be described here.
+ * 
+ * TODO Have you thought about the input restrictions? For example do you allow
+ * ambiguous characters in the input sequence? This should be documented.
+ * 
  * @author kohchuanhock
  * @version 2011.05.09
  */
 public interface AAProperties {
+
 	/**
 	 * @param sequence
 	 * @return Molecular weight of sequence.
 	 */
 	public double getMolecularWeight(ProteinSequence sequence);
+
 	/**
 	 * @param sequence
 	 * @param assumeCysReduced
-	 * @return Extinction coefficient of sequence. Two possible assumption 1) Cys are reduced 2) Cys form cystines.
+	 * @return Extinction coefficient of sequence. Two possible assumption 1)
+	 *         Cys are reduced 2) Cys form cystines.
 	 */
-	public double getExtinctionCoefficient(ProteinSequence sequence, boolean assumeCysReduced);
+	public double getExtinctionCoefficient(ProteinSequence sequence,
+			boolean assumeCysReduced);
+
 	/**
 	 * @param sequence
 	 * @return Instability index of sequence.
 	 */
 	public double getInstabilityIndex(ProteinSequence sequence);
+
 	/**
+	 * TODO name capitalization
+	 * 
 	 * @param sequence
 	 * @return Aliphatic Index of sequence.
 	 */
 	public double getApliphaticindex(ProteinSequence sequence);
+
 	/**
 	 * @param sequence
 	 * @return Grand Average of Hydropathy of sequence.
 	 */
 	public double getAvgHydropathy(ProteinSequence sequence);
+
 	/**
 	 * @param sequence
 	 * @return Isoelectric point of sequence.
 	 */
 	public double getIsoPoint(ProteinSequence sequence);
+
 	/**
+	 * TODO Is it any different from ProteinSequence.getLenght()?
+	 * 
 	 * @param sequence
 	 * @return Length of sequence.
 	 */
 	public double getLength(ProteinSequence sequence);
+
 	/**
 	 * @param sequence
 	 * @return Net charge of sequence
 	 */
 	public double getNetCharge(ProteinSequence sequence);
+
 	/**
+	 * TODO How about using AminoAcid class instead of char? Just make sure
+	 * there is a convenient way for users to define (1) single AA e.g. M, (2)
+	 * subset of AA, e.g. MTA (Met-Ala-Trp). May be defining enum for letter
+	 * codes AA would be good?
+	 * 
+	 * 
+	 * 
 	 * @param sequence
 	 * @param aminoAcidCode
-	 * @return Composition of aminoAcidCode in sequence. Total number of aminoAcidCode / Length of sequence.
+	 * @return Composition of aminoAcidCode in sequence. Total number of
+	 *         aminoAcidCode / Length of sequence.
 	 */
 	public double getEnrichment(ProteinSequence sequence, char aminoAcidCode);
+
 	/**
+	 * TODO use interfaces as return type.
+	 * 
+	 * TODO FYI: do not use Hashtable - this is old badly performing class, use
+	 * HashMap instead
+	 * 
+	 * TODO same as above RE amino acid code
+	 * 
 	 * @param sequence
 	 * @return Hashtable that contains the composition of all amino acids.
 	 */
-	public Hashtable<Character, Double> getAAComposition(ProteinSequence sequence); 
+	public Hashtable<Character, Double> getAAComposition(
+			ProteinSequence sequence);
 }
