ac84ceaccb6a6a52631cde17a2503ae9acfd3a5d
Peter Troshin <to.petr@gmail.com>
2011-05-25 12:01:42 +0000

    Add an example test case. Ah Fu to complete
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@8971 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ biojava3-aa-prop/src/test/java/org/biojava3/aaproperties/PeptidePropertiesImplTester.java
@@ -0,0 +1,41 @@
+package org.biojava3.aaproperties;
+
+import java.util.Map;
+
+import org.biojava3.core.sequence.compound.AminoAcidCompound;
+import org.biojava3.core.sequence.compound.AminoAcidCompoundSet;
+import org.junit.Test;
+import static junit.framework.Assert.*;
+
+public class PeptidePropertiesImplTester {
+
+	/**
+	 * 
+	 */
+	private final String peptide = "MTADGPRELLQLRAAVRHR";
+	
+	@Test
+	public void testAAComposition() { 
+		Map<AminoAcidCompound, Double> composition = PeptideProperties.getAAComposition(peptide);
+		AminoAcidCompound m = AminoAcidCompoundSet.getAminoAcidCompoundSet().getCompoundForString("M");
+		int plen = peptide.length();
+		assertEquals(1.0/plen,composition.get(m));
+		AminoAcidCompound a = AminoAcidCompoundSet.getAminoAcidCompoundSet().getCompoundForString("A");
+		assertEquals(3.0/plen,composition.get(a));
+		//TODO finish for every AA
+		/* 
+		 * TODO This is horrible:  
+		 * AminoAcidCompoundSet.getAminoAcidCompoundSet().getCompoundForString("M");
+		 * either AminoAcidCompoundSet should be modified or adaptor method introduced
+		 */
+	}
+	
+	/*
+	 * TODO 
+	 */
+	@Test
+	public void testApliphaticIndex() {
+		
+	}
+	
+}
