559788c1327e91da8af2c6a08972b5d809b9dfe2
Andreas Prlic <andreas.prlic@gmail.com>
2011-08-24 19:14:59 +0000

    now comparable, serializable
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@9086 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-structure/src/main/java/org/biojava/bio/structure/domain/pdp/Domain.java
+++ biojava3-structure/src/main/java/org/biojava/bio/structure/domain/pdp/Domain.java
@@ -1,10 +1,15 @@
 package org.biojava.bio.structure.domain.pdp;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 import java.util.List;
-
-public class Domain {
+public class Domain implements Comparable<Domain>, Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1293994033102271366L;
 	
 	String id;
 	public int size;
@@ -15,7 +20,6 @@ public class Domain {
 
 	public Domain(){
 		
-
 	}
 	
 
@@ -51,6 +55,17 @@ public class Domain {
 			
 		}
 		return segments.get(pos);
+	}
+
+
+	public int compareTo(Domain other) {
+		
+		
+		if ( this.getId() == null)
+			return 1;
+		if ( other.getId() == null)
+			return -1;
+		return this.getId().compareTo(other.getId());
 	}
 	
 	
