2afaf27051589f2a1815b13ce1435c53b70e8dfd
Andreas Prlic <andreas.prlic@gmail.com>
2011-08-25 22:36:13 +0000

    new test to make sure loading structures from a URL works ok.
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@9097 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ biojava3-structure/src/test/java/org/biojava/bio/structure/TestLoadStructureFromURL.java
@@ -0,0 +1,45 @@
+package org.biojava.bio.structure;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.biojava.bio.structure.align.util.AtomCache;
+
+import junit.framework.TestCase;
+
+
+public class TestLoadStructureFromURL extends TestCase{
+	
+	private static final String FILE_SEPARATOR = System.getProperty("file.separator");
+	public void testLoadStructureFromURL() throws IOException, StructureException{
+		
+		String path = TmpAtomCache.cache.getPath();
+		
+		path += "SUB DIR" + FILE_SEPARATOR;
+		
+		File f = new File(path);
+		if ( ! f.exists()) {
+			System.out.println("making dir with space:" + f);
+			f.mkdir();
+		}
+		AtomCache c = new AtomCache(path, true);
+		// fetch a random small structure
+		
+		c.getStructure("1znf");
+		
+		//and now create a URL for this file
+		
+		File newFile = new File(path+FILE_SEPARATOR+"zn"+ FILE_SEPARATOR + "pdb1znf.ent.gz");
+		
+		URL u = newFile.toURI().toURL();
+		
+		System.out.println(u);
+		
+		Structure s = c.getStructure(u.toString()+"?args=test");
+		
+		System.out.println(s);
+		
+		
+	}
+}
