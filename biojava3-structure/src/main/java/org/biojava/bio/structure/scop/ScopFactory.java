4e0b142737c11f916f50bcadc9ff377b8283492d
Andreas Prlic <andreas.prlic@gmail.com>
2011-09-21 21:01:23 +0000

    From now on we use remote scop calls to fetch the data. No need to waste local memory. Only drawback: port 80 needs to be open.
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@9246 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-structure/src/main/java/org/biojava/bio/structure/scop/ScopFactory.java
+++ biojava3-structure/src/main/java/org/biojava/bio/structure/scop/ScopFactory.java
@@ -2,7 +2,10 @@ package org.biojava.bio.structure.scop;
 
 public class ScopFactory {
 
-	private static ScopDatabase scop = new ScopInstallation();
+	//private static ScopDatabase scop = new ScopInstallation();
+	
+	// by default we now request SCOP via web services -> much less memory consumption
+	private static ScopDatabase scop = new RemoteScopInstallation();
 	
 	public static ScopDatabase getSCOP(){
 		return scop;
