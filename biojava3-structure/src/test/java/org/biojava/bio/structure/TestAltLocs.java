25699c2c5fb6c63229608f1216007de89660a393
Andreas Prlic <andreas.prlic@gmail.com>
2012-02-08 00:26:57 +0000

    unit test for https://redmine.open-bio.org/issues/3307
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@9525 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-structure/src/test/java/org/biojava/bio/structure/TestAltLocs.java
+++ biojava3-structure/src/test/java/org/biojava/bio/structure/TestAltLocs.java
@@ -3,6 +3,7 @@ package org.biojava.bio.structure;
 import java.util.List;
 
 import org.biojava.bio.structure.align.util.AtomCache;
+import org.biojava.bio.structure.io.FileConvert;
 import org.biojava.bio.structure.io.mmcif.chem.PolymerType;
 import org.biojava.bio.structure.io.mmcif.chem.ResidueType;
 import org.biojava.bio.structure.io.mmcif.model.ChemComp;
@@ -29,7 +30,7 @@ public class TestAltLocs extends TestCase {
 				if ( ResidueType.lPeptideLinking.equals(cc.getResidueType()) ||
 						PolymerType.PROTEIN_ONLY.contains(cc.getPolymerType()) ||
 						PolymerType.POLYNUCLEOTIDE_ONLY.contains(cc.getPolymerType())
-				){
+						){
 					if (! ChainImpl.waternames.contains(g.getPDBName())) {
 						//System.out.println(g);
 						groupCount ++;
@@ -39,7 +40,7 @@ public class TestAltLocs extends TestCase {
 					// there are 3 groups in 2CI1 which are non-standard: SNC, KOR, CIT
 					// they are not in the reduced set of standard definitions that will
 					// be shipped in the .jar file.
-					
+
 					// if the download chem comp provider is used
 					// there will be CIT, which is not a peptide, but
 					// should still be counted as a valid HETATOM group...
@@ -83,30 +84,92 @@ public class TestAltLocs extends TestCase {
 		}
 
 	}
-	
+
 	public void test2W72(){
 		try {
 			AtomCache cache = new AtomCache();
 			Structure s = cache.getStructure("2W72");
-			
+
 			Chain a = s.getChainByPDB("A");
-			
+
 			Group val1 = a.getGroupByPDB(ResidueNumber.fromString("1"));
 			Atom ca1 = val1.getAtom(" CA ");
 			assertNotNull(ca1);
-			
+
 			Group lys7 = a.getGroupByPDB(ResidueNumber.fromString("7"));
 			Atom ca7 = lys7.getAtom(" CA ");			
 			assertNotNull(ca7);
-			
+
 			Atom[] caA = StructureTools.getAtomCAArray(a);
-						
+
 			assertEquals(caA.length,141);
-			
+
 		} catch(Exception e){
 			e.printStackTrace();
 			fail(e.getMessage());
 		}
-		
+
 	}
+
+	public void test1U7F(){
+		try {
+			AtomCache cache = new AtomCache();
+			Structure s = cache.getStructure("1U7F");
+
+			Chain c = s.getChainByPDB("B");
+
+			Group g = c.getGroupByPDB(ResidueNumber.fromString("314"));
+			//System.out.println("== original group ==");
+			ensureAllAtomsSameInsCode(g);
+			//System.out.println("== alternate group ==");
+			for ( Group altGroup : g.getAltLocs() ) {
+				ensureAllAtomsSameInsCode(altGroup);	
+			}
+
+		} catch(Exception e){
+			e.printStackTrace();
+			fail(e.getMessage());
+		}	
+	}
+
+	public void test1JXX(){
+		try {
+			AtomCache cache = new AtomCache();
+			Structure structure = cache.getStructure("1JXX");
+
+			Chain chain = structure.getChain(0); // 1JXX example
+
+			Group g = chain.getAtomGroups().get(1); // 1JXX  THR A   2
+			ensureAllAtomsSameInsCode(g);
+			//System.out.println("== alternate group ==");
+			for ( Group altGroup : g.getAltLocs() ) {
+				ensureAllAtomsSameInsCode(altGroup);	
+			}
+			
+		} catch(Exception e){
+			e.printStackTrace();
+			fail(e.getMessage());
+		}	
+
+	}
+
+
+
+	private void ensureAllAtomsSameInsCode(Group g) {
+
+	//	System.out.println(String.format("Group size: %d", g.getAtoms().size()));
+
+		Character defaultAltLoc = null;
+		for (Atom atom : g.getAtoms()) {
+			if ( defaultAltLoc == null) {
+				defaultAltLoc = atom.getAltLoc();
+				continue;
+			}
+		//	System.out.print(atom.toPDB());
+			Character altLoc = atom.getAltLoc();
+
+			assertEquals(defaultAltLoc,altLoc);
+		}
+	}
+
 }
