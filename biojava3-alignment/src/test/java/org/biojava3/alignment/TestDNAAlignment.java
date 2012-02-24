2dd546d38e88ed32a782a9ba1ccaa2008131810e
Hannes Brandstätter-Müller <github@hannes.oib.com>
2012-02-24 04:48:47 +0000

    unified indentation in pom.xml; added some Test cases for DNA Alignments
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@9541 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-alignment/src/test/java/org/biojava3/alignment/TestDNAAlignment.java
+++ biojava3-alignment/src/test/java/org/biojava3/alignment/TestDNAAlignment.java
@@ -1,10 +1,9 @@
 /**
  *                    BioJava development code
  *
- * This code may be freely distributed and modified under the
- * terms of the GNU Lesser General Public Licence.  This should
- * be distributed with the code.  If you do not have a copy,
- * see:
+ * This code may be freely distributed and modified under the terms of the GNU
+ * Lesser General Public Licence. This should be distributed with the code. If
+ * you do not have a copy, see:
  *
  *      http://www.gnu.org/copyleft/lesser.html
  *
@@ -17,7 +16,7 @@
  *
  *      http://www.biojava.org/
  *
- * Created on Oct 5, 2011
+ * Created on Oct 5, 2011 
  * Created by Andreas Prlic
  *
  * @since 3.0.2
@@ -28,54 +27,91 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
-
+import junit.framework.TestCase;
+import org.biojava3.alignment.Alignments.PairwiseSequenceAlignerType;
 import org.biojava3.alignment.template.Profile;
+import org.biojava3.alignment.template.SequencePair;
+import org.biojava3.alignment.template.SubstitutionMatrix;
 import org.biojava3.core.sequence.DNASequence;
+import org.biojava3.core.sequence.compound.AmbiguityDNACompoundSet;
+import org.biojava3.core.sequence.compound.DNACompoundSet;
 import org.biojava3.core.sequence.compound.NucleotideCompound;
 import org.biojava3.core.sequence.io.FastaReaderHelper;
 import org.biojava3.core.util.ConcurrencyTools;
 
-import junit.framework.TestCase;
+public class TestDNAAlignment extends TestCase {
 
-public class TestDNAAlignment extends TestCase{
+    public void testDNAAlignment() {
 
+        try {
+            List<DNASequence> lst = getDNAFASTAFile();
 
-	public void testDNAAlignment(){
+            Profile<DNASequence, NucleotideCompound> profile = Alignments.getMultipleSequenceAlignment(lst);
 
-		try {
-			List<DNASequence> lst = getDNAFASTAFile();
+            assertTrue(profile.getSize() == 10);
 
-			Profile<DNASequence, NucleotideCompound> profile = Alignments.getMultipleSequenceAlignment(lst);
-
-			assertTrue(profile.getSize() == 10);
-
-			assertTrue(profile.getAlignedSequence(1).getSequenceAsString().length() > 50);
-			
-			
-			// here how to print the MSA:
-			
-			//System.out.printf("MSA:%n%s%n", profile);
-		} catch (Exception e){
-			e.printStackTrace();
-			fail(e.getMessage());
-		}
-		ConcurrencyTools.shutdown();
-	}
-
-	private static List<DNASequence> getDNAFASTAFile() throws Exception {
-
-		InputStream inStream = TestDNAAlignment.class.getResourceAsStream(String.format("/dna-fasta.txt")); 
-		LinkedHashMap<String, DNASequence> fastas = FastaReaderHelper.readFastaDNASequence(inStream);
-
-		List<DNASequence> sequences = new ArrayList<DNASequence>();
-
-		for ( String key: fastas.keySet()){
-			DNASequence seq = fastas.get(key);
-			sequences.add(seq);
-		}
-
-		return sequences;
-	}
+            assertTrue(profile.getAlignedSequence(1).getSequenceAsString().length() > 50);
 
 
+            // here how to print the MSA:
+
+            //System.out.printf("MSA:%n%s%n", profile);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+        ConcurrencyTools.shutdown();
+    }
+
+    private static List<DNASequence> getDNAFASTAFile() throws Exception {
+
+        InputStream inStream = TestDNAAlignment.class.getResourceAsStream(String.format("/dna-fasta.txt"));
+        LinkedHashMap<String, DNASequence> fastas = FastaReaderHelper.readFastaDNASequence(inStream);
+
+        List<DNASequence> sequences = new ArrayList<DNASequence>();
+
+        for (String key : fastas.keySet()) {
+            DNASequence seq = fastas.get(key);
+            sequences.add(seq);
+        }
+
+        return sequences;
+    }
+
+    /**
+     * @author brandstaetter
+     */
+    public void testDNAMultipleAlignmentWithAmbiguity() {
+
+        DNASequence target = new DNASequence("ACTGACGTGTAGCTGACTGA", DNACompoundSet.getDNACompoundSet());
+        DNASequence query = new DNASequence("ACTGACGTGTAGCTGACTGTA", DNACompoundSet.getDNACompoundSet());
+        // fails when used instead: DNASequence query = new DNASequence("ACTGACGTGTAGCTGACTGTA", AmbiguityDNACompoundSet.getDNACompoundSet());
+
+        List<DNASequence> lst = new ArrayList<DNASequence>();
+        lst.add(target);
+        lst.add(query);
+
+        Profile<DNASequence, NucleotideCompound> profile = Alignments.getMultipleSequenceAlignment(lst);
+
+        assertEquals(2, profile.getSize());
+
+        assertTrue(profile.getAlignedSequence(1).getSequenceAsString().length() > 20);
+    }
+
+    /**
+     * @author brandstaetter
+     */
+    public void testDNAPairwiseAlignmentWithAmbiguity() {
+        DNASequence target = new DNASequence("ACTGACGTGTAGCTGACTGA", DNACompoundSet.getDNACompoundSet());
+        DNASequence query = new DNASequence("ACTGACGTGTAGCTGACTGG", DNACompoundSet.getDNACompoundSet());
+        // fails when used instead: DNASequence query = new DNASequence("ACTGACGTGTAGCTGACTGT", AmbiguityDNACompoundSet.getDNACompoundSet());
+        SubstitutionMatrix<NucleotideCompound> matrix = SubstitutionMatrixHelper.getNuc4_4();
+        SimpleGapPenalty gapP = new SimpleGapPenalty();
+        gapP.setOpenPenalty((short) 5);
+        gapP.setExtensionPenalty((short) 2);
+        SequencePair<DNASequence, NucleotideCompound> psa = Alignments.getPairwiseAlignment(query, target, PairwiseSequenceAlignerType.LOCAL, gapP, matrix);
+        
+        assertEquals(19, psa.getNumIdenticals());
+        //assertEquals(0, psa.getNumSimilars());
+    }
 }
