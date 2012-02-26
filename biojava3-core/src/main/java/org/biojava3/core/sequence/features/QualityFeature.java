176d0fab57cc20d86b0dc3f0474748e1e37d9f2a
Hannes Brandstätter-Müller <github@hannes.oib.com>
2012-02-26 04:02:44 +0000

    added a QualityFeature to hold quality information in DNASequences
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@9543 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ biojava3-core/src/main/java/org/biojava3/core/sequence/features/QualityFeature.java
@@ -0,0 +1,72 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ * Created on 01-21-2010
+ */
+package org.biojava3.core.sequence.features;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.biojava3.core.sequence.template.AbstractSequence;
+import org.biojava3.core.sequence.template.Compound;
+
+/**
+ * DNA Sequences produced by modern sequencers usually have quality informaion
+ * attached to them. This feature allows to store the information directly in
+ * the DNASequence
+ *
+ * @since 3.0.3
+ * @author brandstaetter
+ */
+public class QualityFeature<S extends AbstractSequence<C>, C extends Compound> extends AbstractFeature<S, C> {
+
+    private List<Number> qualities = new ArrayList<Number>();
+
+    /**
+     *
+     * @param type
+     * @param source
+     */
+    public QualityFeature(String type, String source) {
+        super(type, source);
+    }
+
+    /**
+     * @return the qualities
+     */
+    public List<Number> getQuantities() {
+        return qualities;
+    }
+
+    /**
+     * @param qualities the qualities to set
+     */
+    public void setQuantities(List<Number> qualities) {
+        this.qualities = qualities;
+    }
+
+    /**
+     *
+     * @param bioindex
+     * @return
+     */
+    public Number getQualityAt(int bioindex) {
+        return qualities.get(bioindex - 1);
+    }
+}
