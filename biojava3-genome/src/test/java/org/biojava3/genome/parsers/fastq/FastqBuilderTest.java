9e64084c93776a5848173a0036dff2c65c8957e2
Hannes Brandstätter-Müller <github@hannes.oib.com>
2012-03-12 05:07:39 +0000

    undid some unnecessary reformatting that made the code harder to read in FastqBuilderTest.java
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@9568 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-genome/src/test/java/org/biojava3/genome/parsers/fastq/FastqBuilderTest.java
+++ biojava3-genome/src/test/java/org/biojava3/genome/parsers/fastq/FastqBuilderTest.java
@@ -20,9 +20,6 @@
  */
 package org.biojava3.genome.parsers.fastq;
 
-import org.biojava3.genome.parsers.fastq.FastqVariant;
-import org.biojava3.genome.parsers.fastq.FastqBuilder;
-import org.biojava3.genome.parsers.fastq.Fastq;
 import junit.framework.TestCase;
 
 /**
@@ -47,7 +44,11 @@ public final class FastqBuilderTest extends TestCase {
 
     public void testBuildNullDescription() {
         try {
-            FastqBuilder fastqBuilder = new FastqBuilder().withDescription(null).withSequence("sequence").withQuality("quality_").withVariant(FastqVariant.FASTQ_SOLEXA);
+            FastqBuilder fastqBuilder = new FastqBuilder()
+                    .withDescription(null)
+                    .withSequence("sequence")
+                    .withQuality("quality_")
+                    .withVariant(FastqVariant.FASTQ_SOLEXA);
 
             fastqBuilder.build();
             fail("build null description expected IllegalArgumentException");
@@ -58,7 +59,11 @@ public final class FastqBuilderTest extends TestCase {
 
     public void testBuildNullSequence() {
         try {
-            FastqBuilder fastqBuilder = new FastqBuilder().withDescription("description").withSequence(null).withQuality("quality_").withVariant(FastqVariant.FASTQ_SOLEXA);
+            FastqBuilder fastqBuilder = new FastqBuilder()
+                    .withDescription("description")
+                    .withSequence(null)
+                    .withQuality("quality_")
+                    .withVariant(FastqVariant.FASTQ_SOLEXA);
 
             fastqBuilder.build();
             fail("build null sequence expected IllegalArgumentException");
@@ -69,7 +74,11 @@ public final class FastqBuilderTest extends TestCase {
 
     public void testBuildNullAppendSequence() {
         try {
-            FastqBuilder fastqBuilder = new FastqBuilder().withDescription("description").appendSequence(null).withQuality("quality_").withVariant(FastqVariant.FASTQ_SOLEXA);
+            FastqBuilder fastqBuilder = new FastqBuilder()
+                    .withDescription("description")
+                    .appendSequence(null)
+                    .withQuality("quality_")
+                    .withVariant(FastqVariant.FASTQ_SOLEXA);
 
             fastqBuilder.build();
             fail("build null append sequence expected IllegalArgumentException");
@@ -80,7 +89,11 @@ public final class FastqBuilderTest extends TestCase {
 
     public void testBuildNullQuality() {
         try {
-            FastqBuilder fastqBuilder = new FastqBuilder().withDescription("description").withSequence("sequence").withQuality(null).withVariant(FastqVariant.FASTQ_SOLEXA);
+            FastqBuilder fastqBuilder = new FastqBuilder()
+                    .withDescription("description")
+                    .withSequence("sequence")
+                    .withQuality(null)
+                    .withVariant(FastqVariant.FASTQ_SOLEXA);
 
             fastqBuilder.build();
             fail("build null quality expected IllegalArgumentException");
@@ -91,7 +104,11 @@ public final class FastqBuilderTest extends TestCase {
 
     public void testBuildNullAppendQuality() {
         try {
-            FastqBuilder fastqBuilder = new FastqBuilder().withDescription("description").withSequence("sequence").appendQuality(null).withVariant(FastqVariant.FASTQ_SOLEXA);
+            FastqBuilder fastqBuilder = new FastqBuilder()
+                    .withDescription("description")
+                    .withSequence("sequence")
+                    .appendQuality(null)
+                    .withVariant(FastqVariant.FASTQ_SOLEXA);
 
             fastqBuilder.build();
             fail("build null append quality expected IllegalArgumentException");
@@ -102,7 +119,11 @@ public final class FastqBuilderTest extends TestCase {
 
     public void testBuildNullVariant() {
         try {
-            FastqBuilder fastqBuilder = new FastqBuilder().withDescription("description").withSequence("sequence").withQuality("quality_").withVariant(null);
+            FastqBuilder fastqBuilder = new FastqBuilder()
+                    .withDescription("description")
+                    .withSequence("sequence")
+                    .withQuality("quality_")
+                    .withVariant(null);
 
             fastqBuilder.build();
             fail("build null variant expected IllegalArgumentException");
@@ -113,7 +134,10 @@ public final class FastqBuilderTest extends TestCase {
 
     public void testBuildMissingDescription() {
         try {
-            FastqBuilder fastqBuilder = new FastqBuilder().withSequence("sequence").withQuality("quality_").withVariant(FastqVariant.FASTQ_SOLEXA);
+            FastqBuilder fastqBuilder = new FastqBuilder()
+                    .withSequence("sequence")
+                    .withQuality("quality_")
+                    .withVariant(FastqVariant.FASTQ_SOLEXA);
 
             fastqBuilder.build();
             fail("build missing description expected IllegalStateException");
@@ -124,7 +148,10 @@ public final class FastqBuilderTest extends TestCase {
 
     public void testBuildMissingSequence() {
         try {
-            FastqBuilder fastqBuilder = new FastqBuilder().withDescription("description").withQuality("quality_").withVariant(FastqVariant.FASTQ_SOLEXA);
+            FastqBuilder fastqBuilder = new FastqBuilder()
+                    .withDescription("description")
+                    .withQuality("quality_")
+                    .withVariant(FastqVariant.FASTQ_SOLEXA);
 
             fastqBuilder.build();
             fail("build missing sequence expected IllegalStateException");
@@ -135,7 +162,10 @@ public final class FastqBuilderTest extends TestCase {
 
     public void testBuildMissingQuality() {
         try {
-            FastqBuilder fastqBuilder = new FastqBuilder().withDescription("description").withSequence("sequence").withVariant(FastqVariant.FASTQ_SOLEXA);
+            FastqBuilder fastqBuilder = new FastqBuilder()
+                    .withDescription("description")
+                    .withSequence("sequence")
+                    .withVariant(FastqVariant.FASTQ_SOLEXA);
 
             fastqBuilder.build();
             fail("build missing quality expected IllegalStateException");
@@ -145,7 +175,10 @@ public final class FastqBuilderTest extends TestCase {
     }
 
     public void testBuildDefaultVariant() {
-        FastqBuilder fastqBuilder = new FastqBuilder().withDescription("description").withSequence("sequence").withQuality("quality_");
+        FastqBuilder fastqBuilder = new FastqBuilder()
+                .withDescription("description")
+                .withSequence("sequence")
+                .withQuality("quality_");
 
         Fastq fastq = fastqBuilder.build();
         assertEquals("description", fastqBuilder.getDescription());
@@ -157,7 +190,11 @@ public final class FastqBuilderTest extends TestCase {
     }
 
     public void testBuild() {
-        FastqBuilder fastqBuilder = new FastqBuilder().withDescription("description").withSequence("sequence").withQuality("quality_").withVariant(FastqVariant.FASTQ_SOLEXA);
+        FastqBuilder fastqBuilder = new FastqBuilder()
+                .withDescription("description")
+                .withSequence("sequence")
+                .withQuality("quality_")
+                .withVariant(FastqVariant.FASTQ_SOLEXA);
         Fastq fastq = fastqBuilder.build();
         assertEquals("description", fastqBuilder.getDescription());
         assertTrue(fastqBuilder.sequenceAndQualityLengthsMatch());
@@ -168,7 +205,12 @@ public final class FastqBuilderTest extends TestCase {
     }
 
     public void testBuildAppendSequence() {
-        FastqBuilder fastqBuilder = new FastqBuilder().withDescription("description").appendSequence("seq").appendSequence("uence").withQuality("quality_").withVariant(FastqVariant.FASTQ_SOLEXA);
+        FastqBuilder fastqBuilder = new FastqBuilder()
+                .withDescription("description")
+                .appendSequence("seq")
+                .appendSequence("uence")
+                .withQuality("quality_")
+                .withVariant(FastqVariant.FASTQ_SOLEXA);
         Fastq fastq = fastqBuilder.build();
         assertEquals("description", fastqBuilder.getDescription());
         assertTrue(fastqBuilder.sequenceAndQualityLengthsMatch());
@@ -179,7 +221,12 @@ public final class FastqBuilderTest extends TestCase {
     }
 
     public void testBuildAppendQuality() {
-        FastqBuilder fastqBuilder = new FastqBuilder().withDescription("description").withSequence("sequence").appendQuality("qual").appendQuality("ity_").withVariant(FastqVariant.FASTQ_SOLEXA);
+        FastqBuilder fastqBuilder = new FastqBuilder()
+                .withDescription("description")
+                .withSequence("sequence")
+                .appendQuality("qual")
+                .appendQuality("ity_")
+                .withVariant(FastqVariant.FASTQ_SOLEXA);
         Fastq fastq = fastqBuilder.build();
         assertEquals("description", fastqBuilder.getDescription());
         assertTrue(fastqBuilder.sequenceAndQualityLengthsMatch());
@@ -191,7 +238,11 @@ public final class FastqBuilderTest extends TestCase {
 
     public void testBuildNonMatchingSequenceQualityScoreLengths0() {
         try {
-            FastqBuilder fastqBuilder = new FastqBuilder().withDescription("description").withSequence("01234").withQuality("0123").withVariant(FastqVariant.FASTQ_SOLEXA);
+            FastqBuilder fastqBuilder = new FastqBuilder()
+                    .withDescription("description")
+                    .withSequence("01234")
+                    .withQuality("0123")
+                    .withVariant(FastqVariant.FASTQ_SOLEXA);
 
             fastqBuilder.build();
             fail("build sequence length > quality length expected IllegalStateException");
@@ -202,7 +253,11 @@ public final class FastqBuilderTest extends TestCase {
 
     public void testBuildNonMatchingSequenceQualityScoreLengths1() {
         try {
-            FastqBuilder fastqBuilder = new FastqBuilder().withDescription("description").withSequence("0123").withQuality("01234").withVariant(FastqVariant.FASTQ_SOLEXA);
+            FastqBuilder fastqBuilder = new FastqBuilder()
+                    .withDescription("description")
+                    .withSequence("0123")
+                    .withQuality("01234")
+                    .withVariant(FastqVariant.FASTQ_SOLEXA);
 
             fastqBuilder.build();
             fail("build sequence length < quality length expected IllegalStateException");
@@ -212,7 +267,10 @@ public final class FastqBuilderTest extends TestCase {
     }
 
     public void testBuildMultiple() {
-        FastqBuilder fastqBuilder = new FastqBuilder().withDescription("description").withQuality("quality__").withVariant(FastqVariant.FASTQ_SOLEXA);
+        FastqBuilder fastqBuilder = new FastqBuilder()
+                .withDescription("description")
+                .withQuality("quality__")
+                .withVariant(FastqVariant.FASTQ_SOLEXA);
 
         for (int i = 0; i < 10; i++) {
             Fastq fastq = fastqBuilder.withSequence("sequence" + i).build();
