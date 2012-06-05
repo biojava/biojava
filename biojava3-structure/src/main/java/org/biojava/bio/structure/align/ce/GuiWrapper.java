4d863caa62015b25137cf5c81ecb3e468c4ef035
Andreas Prlic <andreas.prlic@gmail.com>
2012-06-05 23:49:13 +0000

    new wrapper method for showing DB results
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@9688 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-structure/src/main/java/org/biojava/bio/structure/align/ce/GuiWrapper.java
+++ biojava3-structure/src/main/java/org/biojava/bio/structure/align/ce/GuiWrapper.java
@@ -1,5 +1,6 @@
 package org.biojava.bio.structure.align.ce;
 
+import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
@@ -10,6 +11,7 @@ import org.biojava.bio.structure.Atom;
 import org.biojava.bio.structure.Group;
 import org.biojava.bio.structure.Structure;
 import org.biojava.bio.structure.align.model.AFPChain;
+import org.biojava.bio.structure.align.util.UserConfiguration;
 import org.biojava.bio.structure.jama.Matrix;
 
 /** A class to wrap some of the strucutre.gui classes using Reflection
@@ -141,6 +143,35 @@ public class GuiWrapper {
 
 		return atoms;
 
+	}
+	
+	public static void showDBResults(StartupParameters params) {
+		//System.err.println("not implemented full yet");
+		
+		// We want to do this, but because we don't know if structure-gui.jar is in the classpath we use reflection to hide the calls
+		
+		UserConfiguration config = UserConfiguration.fromStartupParams(params);
+		
+		String tableClass = "org.biojava.bio.structure.align.gui.DBResultTable";
+		
+		try {
+			Class c = Class.forName(tableClass);
+			Object table = c.newInstance();
+			
+			Method show = c.getMethod("show", new Class[]{File.class, UserConfiguration.class });
+		
+			show.invoke(table, new File(params.getShowDBresult()),config);
+			
+		} catch (Exception e){
+			e.printStackTrace();
+			
+			System.err.println("Probably structure-gui.jar is not in the classpath, can't show results...");
+		}
+		
+		//DBResultTable table = new DBResultTable();
+		
+		//table.show(new File(params.getShowDBresult()),config);
+		
 	}
 
 }
