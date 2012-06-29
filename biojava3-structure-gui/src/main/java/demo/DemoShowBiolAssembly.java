ef407cf385110a52efa54492fef266f7b379854a
Andreas Prlic <andreas.prlic@gmail.com>
2012-06-29 20:56:04 +0000

    more demos for visualizing biological assemblies
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@9741 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-structure-gui/src/main/java/demo/DemoShowBiolAssembly.java
+++ biojava3-structure-gui/src/main/java/demo/DemoShowBiolAssembly.java
@@ -15,10 +15,20 @@ public class DemoShowBiolAssembly {
 		try{
 			// see also: http://www.pdb.org/pdb/101/static101.do?p=education_discussion/Looking-at-Structures/bioassembly_tutorial.html
 			// good examples: 1stp 1gav 1hv4 1hho 7dfr 3fad  1qqp
+
+			// assembly 0 ... asym Unit
+			// assembly 1 ... the first bio assembly
 			
-			Structure bioAssembly = StructureIO.getBiologicalAssembly("1stp",0);
+			// Various interesting symmetries: (see Lawson, 2008)
+			// Circular    - 1TJA
+			// Dihedral    - 1ei7
+			// Icosahedral - 1a34
+			// Helical     - 1cgm
+			
+			Structure bioAssembly = StructureIO.getBiologicalAssembly("1stp",1);
 			System.out.println(bioAssembly);
 			
+						
 			StructureAlignmentJmol jmolPanel = new StructureAlignmentJmol();
 			//jmolPanel.evalString("set autobond=false");
 			jmolPanel.setStructure(bioAssembly);
