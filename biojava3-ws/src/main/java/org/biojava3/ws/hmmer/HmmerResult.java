a47a7d216e3ef01e7bc492cfb73f81a19bac30d0
Andreas Prlic <andreas.prlic@gmail.com>
2012-07-23 18:34:21 +0000

    PDBWW-4789 entityView is now linked to structure summary page!
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@9749 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-ws/src/main/java/org/biojava3/ws/hmmer/HmmerResult.java
+++ biojava3-ws/src/main/java/org/biojava3/ws/hmmer/HmmerResult.java
@@ -125,4 +125,47 @@ public class HmmerResult implements Comparable<HmmerResult>, Serializable{
 		return false;
 	}
 	
+	public int getOverlapLength(HmmerResult other){
+		
+		int overlap = 0;
+		for ( HmmerDomain d1 : getDomains()){
+			for (HmmerDomain d2 : other.getDomains()){
+				overlap += getOverlap(d1, d2);
+			}
+		}
+		return overlap;
+		
+	}
+	
+	private int getOverlap(HmmerDomain one, HmmerDomain other){
+		int xs = one.getSqFrom();
+		int ys = one.getSqTo();
+		int as = other.getSqFrom();
+		int bs = other.getSqTo();
+
+		int overlap = 0;
+		//1:
+		
+		if ((( xs< as)  && ( as<ys)) || ((xs < bs) && ( bs <= ys)) || (as<xs && ys<bs)) {
+			
+			//2:
+
+			if ( xs < as) {
+				if ( ys < bs) 
+					overlap = ys-as;
+				else
+					overlap = bs-as;
+			} else {
+				if  ( ys < bs) 
+					overlap = ys -xs;
+				else 
+					overlap = bs - xs;
+
+			} 
+
+		}
+
+		return overlap;
+	}
+	
 }
