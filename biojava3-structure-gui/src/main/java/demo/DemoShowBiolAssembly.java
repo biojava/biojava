a5d9b0ba8b686efa021c0f97cedf19a5fad94000
Andreas Prlic <andreas.prlic@gmail.com>
2012-11-20 01:29:17 +0000

    more docu and nice example
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@9901 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-structure-gui/src/main/java/demo/DemoShowBiolAssembly.java
+++ biojava3-structure-gui/src/main/java/demo/DemoShowBiolAssembly.java
@@ -1,8 +1,8 @@
 package demo;
 
-
 import org.biojava.bio.structure.Structure;
 import org.biojava.bio.structure.align.gui.jmol.StructureAlignmentJmol;
+import org.biojava.bio.structure.quaternary.io.BioUnitDataProviderFactory;
 
 import org.biojava3.structure.StructureIO;
 
@@ -10,14 +10,14 @@ public class DemoShowBiolAssembly {
 
 	public static void main(String[] args){
 
-		
-
 		try{
 			// see also: http://www.pdb.org/pdb/101/static101.do?p=education_discussion/Looking-at-Structures/bioassembly_tutorial.html
 			// good examples: 1stp 1gav 1hv4 1hho 7dfr 3fad  1qqp
 
 			// assembly 0 ... asym Unit
 			// assembly 1 ... the first bio assembly
+			// example 1fah has  2 assemblies (two copies of the assembly in asymmetric unit)
+			
 			
 			// Various interesting symmetries: (see Lawson, 2008)
 			// Circular    - 1TJA
@@ -25,10 +25,14 @@ public class DemoShowBiolAssembly {
 			// Icosahedral - 1a34
 			// Helical     - 1cgm
 			
-			Structure bioAssembly = StructureIO.getBiologicalAssembly("1stp",1);
-			System.out.println(bioAssembly);
 			
-						
+			// DNA 173D .. 2
+			
+			BioUnitDataProviderFactory.setBioUnitDataProvider(BioUnitDataProviderFactory.pdbProviderClassName);
+			//Structure bioAssembly = StructureIO.getBiologicalAssembly("4A1I",2);	
+			
+			Structure bioAssembly = StructureIO.getBiologicalAssembly("1ei7",1);
+									
 			StructureAlignmentJmol jmolPanel = new StructureAlignmentJmol();
 			//jmolPanel.evalString("set autobond=false");
 			jmolPanel.setStructure(bioAssembly);
@@ -37,8 +41,6 @@ public class DemoShowBiolAssembly {
 			jmolPanel.evalString("select * ; color structure ; spacefill off; wireframe off; backbone off; cartoon on; select ligands ; spacefill 0.4; color cpk;");
 			
 			System.out.println("done!");
-			//quaternaryBuilder.getBioUnitTransformationList()
-
 
 		} catch (Exception e) {
 			e.printStackTrace();
