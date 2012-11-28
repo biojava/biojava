0d7f1412e6039fed546a4bc00b498ed2a23e0a0a
Andreas Prlic <andreas.prlic@gmail.com>
2012-11-28 17:44:28 +0000

    default provider is PDB based...
    git-svn-id: http://code.open-bio.org/repos/biojava/biojava-live/trunk@9918 7c6358e6-4a41-0410-a743-a5b2a554c398

--- biojava3-structure/src/main/java/org/biojava/bio/structure/quaternary/io/BioUnitDataProviderFactory.java
+++ biojava3-structure/src/main/java/org/biojava/bio/structure/quaternary/io/BioUnitDataProviderFactory.java
@@ -10,7 +10,7 @@ public class BioUnitDataProviderFactory {
 	
 	public static final String fileBasedProviderClassName = "org.biojava.bio.structure.quaternary.io.FileBasedPDBBioUnitDataProvider";
 	
-	public static String DEFAULT_PROVIDER_CLASSNAME =  mmcifProviderClassName;
+	public static String DEFAULT_PROVIDER_CLASSNAME =  pdbProviderClassName;
 	
 	private static String providerClassName = DEFAULT_PROVIDER_CLASSNAME;
 	
