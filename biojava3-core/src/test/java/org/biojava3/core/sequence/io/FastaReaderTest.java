abe7d98d9606ef790ef33a53428599dd3bc170ac
aalhossary <amr_alhossary@hotmail.com>
2013-04-23 14:22:03 +0800

    Added process(int max) which tries to parse maximum max records from the open File or InputStream, and leaves the underlying resource open. Subsequent calls to the same method resumes parsing. This is particularly useful when dealing with very big data files, (e.g. NCBI nr database), which can't fit into memory and will take long time before the first result is available.
    Signed-off-by: aalhossary <amr_alhossary@hotmail.com>

--- biojava3-core/src/test/java/org/biojava3/core/sequence/io/FastaReaderTest.java
+++ biojava3-core/src/test/java/org/biojava3/core/sequence/io/FastaReaderTest.java
@@ -86,4 +86,61 @@ public class FastaReaderTest {
 		assertEquals(seqNum,283);
 	}
 	
+	@Test
+	public void processIntTest() throws Exception {
+		System.out.println("process(int)");
+		InputStream inStream = this.getClass().getResourceAsStream("/PF00104_small.fasta");
+		assertNotNull(inStream);
+		FastaReader<ProteinSequence,AminoAcidCompound> fastaReader = new FastaReader<ProteinSequence,AminoAcidCompound>(inStream, new GenericFastaHeaderParser<ProteinSequence,AminoAcidCompound>(), new ProteinSequenceCreator(AminoAcidCompoundSet.getAminoAcidCompoundSet()));
+		LinkedHashMap<String,ProteinSequence> proteinSequences = fastaReader.process(200);
+
+		//Should have 200 sequences
+		//System.out.println("Expecting 200 got " + proteinSequences.size());
+		assertEquals(proteinSequences.size() ,  200 );
+		
+		int seqNum = 0;
+		for(String id:proteinSequences.keySet()) {
+			ProteinSequence proteinSequence = proteinSequences.get(id);
+			switch(seqNum) {
+			case 0:
+				assertEquals(proteinSequence.getAccession().getID(),"A2D504_ATEGE/1-46");
+				assertEquals(proteinSequence.getSequenceAsString(),"-----------------FK-N----LP-LED----------------Q----ITL--IQY-----------SWM----------------------CL-SSFA------LSWRSYK---HTNSQFLYFAPDLVF-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+				break;
+			case 199:
+				assertEquals(proteinSequence.getAccession().getID(),"Q5F0P7_HUMAN/248-428");
+				assertEquals(proteinSequence.getSequenceAsString(),"DRELVVIIGWAKHI-PGFS-S----LS-LGD----------------Q----MSL--LQS-----------AWM----------------------EI-LILG------IVYRSLP---YDDKLVYAEDYIMD-------------------------------------------------------------------------------------------------------------------------------------------------------------EEHSRLAGLLELYRAILQLV-RRY-KKL-K-VEKEEF--------------------VTLKA-LALA-NSDSMY-------------------------------------------IEDL--EAVQKLQDLLHEALQD-Y-----ELS---------QR---HE----------------------------EP------W--------RTG-KLLLTLPL-LRQTA-----------------AKA-VQHF--YSVKLQGKV--PMH--KLF-------LEM---");
+				break;
+			}
+			seqNum++;
+		}
+		assertEquals(seqNum,200);
+		
+		//Should have 83 sequences
+		proteinSequences = fastaReader.process(200);
+		assertEquals(proteinSequences.size() , 83 );
+		seqNum = 0;
+		for(String id:proteinSequences.keySet()) {
+			ProteinSequence proteinSequence = proteinSequences.get(id);
+			switch(seqNum) {
+			case 0:
+				assertEquals(proteinSequence.getAccession().getID(),"RARA_CANFA/233-413");
+				assertEquals(proteinSequence.getSequenceAsString(), "TKCIIKTVEFAKQL-PGFT-T----LT-IAD----------------Q----ITL--LKA-----------ACL----------------------DI-LILR------ICTRYTP---EQDTMTFSEGLTLN-------------------------------------------------------------------------------------------------------------------------------------------------------------RTQMHKAGFGPLTDLVFAFA-NQL-LPL-E-MDDAET--------------------GLLSA-ICLI-CGDRQD-------------------------------------------LEQP--DRVDMLQEPLLEALKV-Y-----VRK---------RR---PS----------------------------RP------H--------MFP-KMLMKITD-LRSIS-----------------AKG-AERV--ITLKMEIPG--SMP--PLI-------QEM---");
+				break;
+			case 81:
+				//System.out.println(proteinSequence.getAccession());
+				//System.out.println(proteinSequence.getSequenceAsString());
+				assertEquals(proteinSequence.getAccession().getID(),"Q9PU76_CRONI/141-323");
+				assertEquals(proteinSequence.getSequenceAsString(),"VETVTELTEFAKSI-PGFS-N----LD-LND----------------Q----VTL--LKY-----------GVY----------------------EA-IFAM------LASVMNK---DGMPVAYGNGFITRE------------------------------------------------------------------------------------------------------------------------------------------------------------FLKSLRKPFCDIMEPKFDFA-MKF-NSL-E-LDDSDI--------------------SLFVA-AIIC-CGDRPG-------------------------------------------LVNV--GHIEKMQESIVHVLKL-H-----LQN---------NH---PD----------------------------DI------F--------LFP-KLLQKMAD-LRQLV-----------------TEH-AQLV--QIIKK---TESDAHLHPLL-------QEI---");
+				break;
+			case 82:
+				assertEquals(proteinSequence.getAccession().getID(),"Q98SJ1_CHICK/15-61");
+				assertEquals(proteinSequence.getSequenceAsString(),"---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------Q-----------------NW------Q--------RFY-QLTKLLDS-MHDVV-----------------ENL-LSFC--FQTFLDKSM--SIEFPEML-------AEI---");
+				break;
+			}
+			seqNum++;
+		}
+		assertEquals(seqNum,83);
+		fastaReader.close();
+		inStream.close();
+	}
 }
