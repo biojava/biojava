f7d738f790fe23364054a5533689d36350f6e847
dmyersturnbull <dmyersturnbull@gmail.com>
2013-06-08 22:42:23 -0700

    Made ResidueRange more robust, and gave it some methods for iterating, finding, and checking containment.

--- biojava3-structure/src/main/java/org/biojava/bio/structure/ResidueRange.java
+++ biojava3-structure/src/main/java/org/biojava/bio/structure/ResidueRange.java
@@ -24,21 +24,16 @@
 package org.biojava.bio.structure;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
-
-import org.biojava.bio.structure.Atom;
-import org.biojava.bio.structure.ResidueNumber;
-import org.biojava.bio.structure.Structure;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * A chain, a start residue, and an end residue. May also store a length value. Because of insertion codes, this length
- * is not necessarily end-start. Since residue numbers are unique within a PDB file, each ResidueNumber in a
- * {@link Structure} can be mapped to a unique ATOM record in that Structure's PDB file. ResidueRange provides a static
- * method {@link #getAminoAcidPositions(Atom[])} that returns a {@link HashMap} that maps each {@link ResidueNumber} to
- * its position in the PDB file's ATOM records. It also provides static methods for working with this map. Note that
- * these are defined as static utility methods only for performance reasons; it is not economical to store this data in
- * each ResidueRange.
+ * is not necessarily {@code end − start}.
  * 
  * @author dmyerstu
  * @see ResidueNumber
@@ -74,14 +69,25 @@ public class ResidueRange {
 	/**
 	 * @param s
 	 *            A string of the form chain_start-end. For example: <code>A.5-100</code>.
-	 * @return The unique ResidueRange corresponding to {@code s}.
+	 * @return The unique ResidueRange corresponding to {@code s}, or null if it is a whole chain
 	 */
 	public static ResidueRange parse(String s) {
 		char chain = s.charAt(0);
-		String[] parts = s.substring(2).split("-");
-		ResidueNumber start = ResidueNumber.fromString(parts[0]);
+		ResidueNumber start, end;
+		if (s.length() > 2) {
+			Pattern pattern = Pattern.compile("^([-]?[\\d]+[\\w]?)-([-]?[\\d]+[\\w]?)$");
+			Matcher matcher = pattern.matcher(s.substring(2));
+			matcher.find();
+			try {
+				start = ResidueNumber.fromString(matcher.group(1));
+				end = ResidueNumber.fromString(matcher.group(2));
+			} catch (IllegalStateException e) {
+				throw new IllegalArgumentException("Range " + s + " was not valid", e);
+			}
+		} else {
+			return null; // this is ok
+		}
 		start.setChainId(String.valueOf(chain));
-		ResidueNumber end = ResidueNumber.fromString(parts[1]);
 		end.setChainId(String.valueOf(chain));
 		return new ResidueRange(chain, start, end, null);
 	}
@@ -93,6 +99,10 @@ public class ResidueRange {
 	 */
 	public static ResidueRange parse(String s, AtomPositionMap map) {
 		ResidueRange rr = parse(s);
+		if (rr == null) { // whole chain
+			String chain = s.substring(0, 1);
+			rr = new ResidueRange(chain.charAt(0), map.getFirst(chain), map.getLast(chain), null);
+		}
 		int length = map.calcLength(rr.getStart(), rr.getEnd());
 		return new ResidueRange(rr.getChain(), rr.getStart(), rr.getEnd(), length);
 	}
@@ -183,6 +193,132 @@ public class ResidueRange {
 	@Override
 	public String toString() {
 		return chain + "_" + start + "-" + end;
+	}
+
+	/**
+	 * @return True if and only if {@code residueNumber} is within this ResidueRange
+	 */
+	public boolean contains(ResidueNumber residueNumber, AtomPositionMap map) {
+		int pos = map.getPosition(residueNumber);
+		int startPos = map.getPosition(start);
+		int endPos = map.getPosition(end);
+		return pos >= startPos && pos <= endPos;
+	}
+
+	/**
+	 * Returns the ResidueNumber that is at position {@code positionInRange} in <em>this</em> ResidueRange.
+	 * @return The ResidueNumber, or false if it does not exist or is not within this ResidueRange
+	 */
+	public ResidueNumber getResidue(int positionInRange, AtomPositionMap map) {
+		int i = 0;
+		for (Map.Entry<ResidueNumber, Integer> entry : map.getNavMap().entrySet()) {
+			if (i == positionInRange) return entry.getKey();
+			if (contains(entry.getKey(), map)) {
+				i++;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns a new Iterator over every {@link ResidueNumber} in this ResidueRange.
+	 * Stores the contents of {@code map} until the iterator is finished, so calling code should set the iterator to {@code null} if it did not finish.
+	 */
+	public Iterator<ResidueNumber> iterator(final AtomPositionMap map) {
+		// get the length without the side effect of setting it
+		int theLength = 0;
+		if (length == null) {
+			theLength = map.calcLength(start, end);
+		} else {
+			theLength = this.length;
+		}
+		final int length = theLength;
+		return new Iterator<ResidueNumber>() {
+			private ResidueNumber[] residueNumbers = new ResidueNumber[map.getNavMap().size()];
+			private int i = -1;
+			@Override
+			public boolean hasNext() {
+				return i < length;
+			}
+			@Override
+			public ResidueNumber next() {
+				if (i == -1) {
+					residueNumbers = new ResidueNumber[map.getNavMap().size()];
+					int j = 0;
+					for (Map.Entry<ResidueNumber,Integer> entry : map.getNavMap().entrySet()) {
+						residueNumbers[j] = entry.getKey();
+						if (contains(entry.getKey(), map)) {
+							j++;
+						}
+					}
+				}
+				i++;
+				ResidueNumber rn = residueNumbers[i];
+				// let's assume we're not going to use this anymore
+				// destroy array to free memory
+				// we can always reconstruct
+				if (i > length) {
+					residueNumbers = null;
+					i = -1;
+				}
+				return rn;
+			}
+			@Override
+			public void remove() {
+				// do nothing since ResidueRange is not modifiable
+			}
+		};
+	}
+
+	/**
+	 * Returns a new Iterator over every {@link ResidueNumber} in the list of ResidueRanges.
+	 * Stores the contents of {@code map} until the iterator is finished, so calling code should set the iterator to {@code null} if it did not finish.
+	 */
+	public static Iterator<ResidueNumber> multiIterator(final AtomPositionMap map, final ResidueRange... rrs) {
+		return new Iterator<ResidueNumber>() {
+			private int r = 0;
+			private Iterator<ResidueNumber> internal;
+			@Override
+			public boolean hasNext() {
+				if (r == rrs.length - 1) {
+					init();
+					return internal.hasNext();
+				}
+				return true;
+			}
+			private void init() {
+				if (internal == null) {
+					internal = rrs[r].iterator(map);
+				}
+			}
+			@Override
+			public ResidueNumber next() {
+				if (rrs.length == 0) throw new NoSuchElementException();
+				init();
+				if (!hasNext()) throw new NoSuchElementException();
+				if (!internal.hasNext()) {
+					r++;
+					internal = rrs[r].iterator(map);
+				}
+				return internal.next();
+			}
+			@Override
+			public void remove() {
+				// do nothing since ResidueRange is not modifiable
+			}
+		};
+	}
+
+	/**
+	 * Returns a new Iterator over every {@link ResidueNumber} in the list of ResidueRanges.
+	 * Stores the contents of {@code map} until the iterator is finished, so calling code should set the iterator to {@code null} if it did not finish.
+	 */
+	public static Iterator<ResidueNumber> multiIterator(AtomPositionMap map, List<ResidueRange> rrs) {
+		ResidueRange[] ranges = new ResidueRange[rrs.size()];
+		for (int i = 0; i < rrs.size(); i++) {
+			ranges[i] = rrs.get(i);
+		}
+		return multiIterator(map, ranges);
 	}
 
 }
