16c9f814b08c9d7e5243e3dc0e991bb962887bb7
Andreas Prlic <andreas.prlic@gmail.com>
2013-09-12 10:34:36 -0700

    updating the demo how to load mmcif files

--- biojava3-structure/src/main/java/demo/DemoMMCIFReader.java
+++ biojava3-structure/src/main/java/demo/DemoMMCIFReader.java
@@ -31,8 +31,11 @@ import org.biojava.bio.structure.Group;
 import org.biojava.bio.structure.GroupType;
 import org.biojava.bio.structure.ResidueNumber;
 import org.biojava.bio.structure.Structure;
+import org.biojava.bio.structure.StructureTools;
+import org.biojava.bio.structure.align.util.AtomCache;
 import org.biojava.bio.structure.io.MMCIFFileReader;
 import org.biojava.bio.structure.io.StructureIOFile;
+import org.biojava3.structure.StructureIO;
 
 /** An example of how to read MMcif files
  * 
@@ -43,6 +46,42 @@ public class DemoMMCIFReader
 {
 
 	public static void main(String[] args){
+
+		DemoMMCIFReader demo = new DemoMMCIFReader();
+
+		demo.loadSimple();
+
+		//demo.loadFromDirectAccess();
+
+	}
+
+	/** A basic example how to load an mmCif file and get a Structure object
+	 *  
+	 */
+	public void loadSimple(){
+		String pdbId = "4hhb";
+
+		AtomCache cache = new AtomCache();
+		cache.setUseMmCif(true);
+
+		StructureIO.setAtomCache(cache);
+
+		try {
+			Structure s = StructureIO.getStructure(pdbId);
+
+			System.out.println(pdbId + " has nr atoms: " + StructureTools.getNrAtoms(s));
+
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+
+	/** An example demonstrating how to directly use the mmCif file parsing classes. This could potentially be used
+	 * to use the parser to populate a data-structure that is different from the biojava-structure data model.
+	 * 
+	 */
+	public void loadFromDirectAccess(){
 		String pdbId = "1A4W";
 
 		StructureIOFile pdbreader = new MMCIFFileReader();
@@ -50,28 +89,28 @@ public class DemoMMCIFReader
 		try {
 			pdbreader.setAutoFetch(true);
 			Structure s = pdbreader.getStructureById(pdbId);
-			
+
 			Chain h = s.getChainByPDB("H");
 
 			List<Group> ligands = h.getAtomLigands();
-			
+
 			System.out.println("These ligands have been found in chain " + h.getChainID());
-			
+
 			for (Group l:ligands){
 				System.out.println(l);
 			}
-			
+
 			System.out.println("Accessing QWE directly: ");
 			Group qwe = h.getGroupByPDB(new ResidueNumber("H",373,null));
 
 			System.out.println(qwe.getChemComp());
-			
+
 			System.out.println(h.getSeqResSequence());
 			System.out.println(h.getAtomSequence());
 			System.out.println(h.getAtomGroups(GroupType.HETATM));
-			
+
 			System.out.println("Compounds: " + s.getCompounds());
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
