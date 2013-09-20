17f8f8c85ef8f7d808b9c0b8aa729a964c4ed8b8
Spencer Bliven <sbliven@ucsd.edu>
2013-09-19 23:56:28 -0700

    Cleaned up ScopFactory
    Removed multiton pattern. There is a single default instance,
    and then it creates non-singleton instances where needed for
    odd versions.
    
    Supports older versions of SCOP

--- biojava3-structure/src/main/java/org/biojava/bio/structure/scop/ScopFactory.java
+++ biojava3-structure/src/main/java/org/biojava/bio/structure/scop/ScopFactory.java
@@ -1,7 +1,5 @@
 package org.biojava.bio.structure.scop;
 
-import java.util.HashMap;
-import java.util.Map;
 
 
 
@@ -28,104 +26,127 @@ import java.util.Map;
 public class ScopFactory {
 
     // berkeley
-    public static final String VERSION_1_75A = "1.75A";
     public static final String VERSION_1_75B = "1.75B";
+    public static final String VERSION_1_75A = "1.75A";
     // original SCOP
     // latest SCOP release from SCOP website = 1.75;
     public static final String VERSION_1_75 = "1.75";
+    // outdated stable versions
+    public static final String VERSION_1_73 = "1.73";
+    public static final String VERSION_1_71 = "1.71";
+    public static final String VERSION_1_69 = "1.69";
+    public static final String VERSION_1_67 = "1.67";
+    public static final String VERSION_1_65 = "1.65";
+    public static final String VERSION_1_63 = "1.63";
+    public static final String VERSION_1_61 = "1.61";
+    public static final String VERSION_1_59 = "1.59";
+    public static final String VERSION_1_57 = "1.57";
+    public static final String VERSION_1_55 = "1.55";
 
     // by default we now request SCOP via web services -> much less memory consumption
     private static ScopDatabase scop = new RemoteScopInstallation();
-    static Map<String,ScopDatabase> versionedScopDBs = new HashMap<String, ScopDatabase>();
 
+    /**
+     * Get the current default instance
+     * @return
+     */
     public static ScopDatabase getSCOP(){
         return scop;
     }
 
-    /** requests a particular version of SCOP
+    /**
+     * requests a particular version of SCOP.
      *
+     * Where possible, this will be the current default instance.
+     * Otherwise a new instance will be created.
      * @param version
      * @return
      */
     public static ScopDatabase getSCOP(String version){
-        if ( version.equalsIgnoreCase(VERSION_1_75A)) {
-            return getBerkeley_1_75A();
-
+        // assume versions sort lexicographically
+        if(scop.getScopVersion().equalsIgnoreCase(version)) {
+            return scop;
         }
-        else if ( version.equalsIgnoreCase(VERSION_1_75B)) {
-            return getBerkeley_1_75B();
-        } else if ( version.equalsIgnoreCase(VERSION_1_75)) {
-            return getScop_1_75();
-        } else {
-            System.err.println("Unknown SCOP version " + version + " . Returning default");
+        // Default to a local installation
+        return getSCOP(version,true);
+    }
 
+    /**
+     * Gets an instance of the specified scop version. Not that the instance
+     * may or may not be the default instance.
+     *
+     * If useLocalData, the returned instance will be either a
+     * {@link ScopInstallation} or a {@link BerkeleyScopInstallation}. If not,
+     * it will be a {@link RemoteScopInstallation}.
+     * @param version A version number, such as {@link #VERSION_1_75A}
+     * @param useLocalData Whether to use a local installation or a remote installation
+     * @return an
+     */
+    public static ScopDatabase getSCOP(String version, boolean useLocalData){
+        if ( useLocalData) {
+            // Use a local installation
+            // Assume version strings sort lexicographically
+            if ( version.compareToIgnoreCase(VERSION_1_75) > 0 ) {
+                return getBerkeley(version);
+            } else {
+                return getScopInstallation(version);
+            }
+        } else {
+            // Use a remote installation
             return scop;
         }
     }
 
-    public static ScopDatabase getSCOP(String version, boolean useLocalData){
-        if ( useLocalData) {
-            if ( version.equalsIgnoreCase(VERSION_1_75A)) {
-                return getBerkeley_1_75A();
-
-            }
-            else if ( version.equalsIgnoreCase(VERSION_1_75B)) {
-                return getBerkeley_1_75B();
-            } else if  ( version.equalsIgnoreCase(VERSION_1_75)){
-                getScop_1_75();
-            } else {
-                getScop_1_75();
-            }
-        } else {
-            // should to get proxied via Domain service servlet
-            return scop;
-        }
+    /**
+     * Set the default instance to use the specified SCOP version number
+     * @param version A version number, such as {@link #VERSION_1_75A}
+     * @return the new default instance
+     */
+    public static ScopDatabase setScopDatabase(String version) {
+        setScopDatabase(ScopFactory.getSCOP(version));
         return scop;
     }
 
     /**
-     * @param version A version number, such as {@link #VERSION_1_75A}
+     * Make `scop` the default instance for all BioJava
+     * @param scop
      */
-    public static void setScopDatabase(String version) {
-        setScopDatabase(ScopFactory.getSCOP(version));
+    public static void setScopDatabase(ScopDatabase scop){
+        System.out.println("Setting ScopDatabase to type: " + scop.getClass().getName());
+        ScopFactory.scop = scop;
     }
 
-    public static void setScopDatabase(ScopDatabase s){
-        System.out.println("Setting ScopDatabase to type: " + s.getClass().getName());
-        scop = s;
-    }
-
-    private static ScopDatabase getScop_1_75() {
-        ScopInstallation scop = (ScopInstallation)versionedScopDBs.get(VERSION_1_75);
-        if ( scop == null) {
-            scop = new ScopInstallation();
-            scop.setScopVersion(VERSION_1_75);
-            versionedScopDBs.put(VERSION_1_75, scop);
+    /**
+     * Gets a local instance with the specified version.
+     *
+     * Uses the singleton if applicable, or creates a new instance
+     * @param version a version applicable to a ScopInstallation
+     * @return
+     */
+    private static ScopInstallation getScopInstallation(String version) {
+        if( scop instanceof ScopInstallation && scop.getScopVersion() == version) {
+            return (ScopInstallation)scop;
+        } else {
+            ScopInstallation scopInst = new ScopInstallation();
+            scopInst.setScopVersion(version);
+            return scopInst;
         }
-
-        return scop;
-
     }
 
-    private static ScopDatabase getBerkeley_1_75A() {
-        BerkeleyScopInstallation berkeley_1_75A = (BerkeleyScopInstallation) versionedScopDBs.get(VERSION_1_75A);
-
-        if ( berkeley_1_75A == null) {
-            berkeley_1_75A  = new BerkeleyScopInstallation();
-            berkeley_1_75A.setScopVersion(VERSION_1_75A);
-            versionedScopDBs.put(VERSION_1_75A, berkeley_1_75A);
+    /**
+     * Gets a Berkeley instance with the specified version.
+     *
+     * Uses the singleton if applicable, or creates a new instance
+     * @param version
+     * @return
+     */
+    private static BerkeleyScopInstallation getBerkeley(String version) {
+        if( scop instanceof BerkeleyScopInstallation && scop.getScopVersion() == version) {
+            return (BerkeleyScopInstallation)scop;
+        } else {
+            BerkeleyScopInstallation berkeley  = new BerkeleyScopInstallation();
+            berkeley.setScopVersion(version);
+            return berkeley;
         }
-        return berkeley_1_75A;
-    }
-
-    private static ScopDatabase getBerkeley_1_75B() {
-        BerkeleyScopInstallation berkeley_1_75B = (BerkeleyScopInstallation) versionedScopDBs.get(VERSION_1_75B);
-
-        if ( berkeley_1_75B == null) {
-            berkeley_1_75B  = new BerkeleyScopInstallation();
-            berkeley_1_75B.setScopVersion(VERSION_1_75B);
-            versionedScopDBs.put(VERSION_1_75B, berkeley_1_75B);
-        }
-        return berkeley_1_75B;
     }
 }
