1f0ad87c4edf3dd11ad5fdb30ed8eab49f5cf64e
dmyersturnbull <dmyersturnbull@gmail.com>
2013-12-19 18:27:23 -0800

    Working on issue #81, created Substructure and UriIdentifier as two Identifier implementations, and created the utility class Identifier.

---
+++ biojava3-structure/src/main/java/org/biojava/bio/structure/Identifier.java
@@ -0,0 +1,42 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ * Created on December 19, 2013
+ * Author: Douglas Myers-Turnbull
+ */
+
+package org.biojava.bio.structure;
+
+import java.net.URISyntaxException;
+
+/**
+ * A collection of utilities to create {@link StructureIdentifier StructureIdentifiers}.
+ * @author dmyersturnbull
+ */
+public class Identifier {
+
+	public StructureIdentifier loadIdentifier(String id) {
+		try {
+			return new UriIdentifier(id);
+		} catch (URISyntaxException e) {
+			return new Substructure(id);
+		}
+	}
+	
+}
