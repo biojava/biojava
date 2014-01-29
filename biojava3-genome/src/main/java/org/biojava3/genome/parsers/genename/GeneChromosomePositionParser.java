2d16e8421d0a9c79ccd7c610ba9c1d18f5c76cc5
Andreas Prlic <andreas.prlic@gmail.com>
2014-01-29 09:28:50 -0800

    adding support for parsing gene names and chromosome locations for human genes

---
+++ biojava3-genome/src/main/java/org/biojava3/genome/parsers/genename/GeneChromosomePositionParser.java
@@ -0,0 +1,134 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ * created at 28 Jan 2014
+ * Author: ap3 
+ */
+
+package org.biojava3.genome.parsers.genename;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.biojava3.core.util.InputStreamProvider;
+
+/** A parser that parses a file from the UCSC genome browser that contains mapping of gene name to chromosome positions
+ * 
+ * @author Andreas Prlic
+ *
+ */
+public class GeneChromosomePositionParser {
+
+	public static final String DEFAULT_MAPPING_URL="http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/refFlat.txt.gz";
+
+	public static void main(String[] args){
+		try {
+
+			List<GeneChromosomePosition> genePositions=	getChromosomeMappings();
+			System.out.println("got " + genePositions.size() + " gene positions") ;
+			
+			for (GeneChromosomePosition pos : genePositions){
+				if ( pos.getGeneName().equals("FOLH1")) {
+					System.out.println(pos);
+					break;
+				}
+			}
+			
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public static List<GeneChromosomePosition> getChromosomeMappings() throws IOException {
+
+		URL url = new URL(DEFAULT_MAPPING_URL);
+
+		InputStreamProvider prov = new InputStreamProvider();
+
+		InputStream inStream = prov.getInputStream(url);
+
+		return getChromosomeMappings(inStream);
+	}
+
+	public static List<GeneChromosomePosition> getChromosomeMappings(InputStream inStream) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
+
+		ArrayList<GeneChromosomePosition> gcps = new ArrayList<GeneChromosomePosition>();
+
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			GeneChromosomePosition gcp = getGeneChromosomePosition(line);
+			if ( gcp != null)
+				gcps.add(gcp);
+		}
+
+		// since this is a large list, remove empty content.
+		gcps.trimToSize();
+		return gcps;
+	}
+
+	private static GeneChromosomePosition getGeneChromosomePosition(String line) {
+		if ( line == null)
+			return null;
+		String[] spl = line.split("\t");
+
+		if ( spl.length != 11) {
+			System.err.println("Line does not have 11 data items, but " + spl.length);
+			System.err.println(line);
+			return null;
+		}
+
+		GeneChromosomePosition g = new GeneChromosomePosition();
+
+		g.setGeneName(spl[0]);
+		g.setGenebankId(spl[1]);
+		g.setChromosome(spl[2]);
+		g.setOrientation(spl[3].charAt(0));
+		g.setCdsStart(Integer.parseInt(spl[4]));
+		g.setCdsEnd(Integer.parseInt(spl[5]));
+		g.setTranscriptionStart(Integer.parseInt(spl[6]));
+		g.setTranscriptionEnd(Integer.parseInt(spl[7]));
+		g.setExonCount(Integer.parseInt(spl[8]));
+		String exonStarts = spl[9];
+		String exonEnds = spl[10];
+		g.setExonStarts(getIntegerList(exonStarts));
+		g.setExonEnds(getIntegerList(exonEnds));
+
+		//System.out.println(line);
+		//System.out.println(Arrays.asList(spl) + " " + spl.length);
+		return g;
+	}
+
+	private static List<Integer> getIntegerList(String lst){
+		String[] spl = lst.split(",");
+		ArrayList<Integer> l = new ArrayList<Integer>();
+		for (String s : spl){
+			l.add(Integer.parseInt(s));
+		}
+		l.trimToSize();
+		return l;
+	}
+}
---
+++ biojava3-genome/src/main/java/org/biojava3/genome/parsers/genename/GeneNamesParser.java
@@ -0,0 +1,143 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ * created at 28 Jan 2014
+ * Author: Andreas Prlic
+ */
+
+package org.biojava3.genome.parsers.genename;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.biojava3.core.util.InputStreamProvider;
+
+/** Parses a file from the www.genenames.org website that contains a mapping of human gene names to other databases
+ * 
+ * @author Andreas Prlic
+ *
+ */
+public class GeneNamesParser {
+	public static final String DEFAULT_GENENAMES_URL = "http://www.genenames.org/cgi-bin/download?title=HGNC+output+data&hgnc_dbtag=on&col=gd_hgnc_id&col=gd_app_sym&col=gd_app_name&col=gd_status&col=gd_prev_sym&col=gd_prev_name&col=gd_aliases&col=gd_pub_chrom_map&col=gd_pub_acc_ids&col=gd_pub_refseq_ids&col=md_prot_id&status=Approved&status_opt=2&where=((gd_pub_chrom_map%20not%20like%20%27%patch%%27%20and%20gd_pub_chrom_map%20not%20like%20%27%ALT_REF%%27)%20or%20gd_pub_chrom_map%20IS%20NULL)%20and%20gd_locus_group%20%3d%20%27protein-coding%20gene%27&order_by=gd_app_sym_sort&format=text&limit=&submit=submit&.cgifields=&.cgifields=chr&.cgifields=status&.cgifields=hgnc_dbtag";
+
+	/** parses a file from the genenames website 
+	 *    
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		try {
+						
+			List<GeneName> geneNames = getGeneNames();
+						
+			System.out.println("got " + geneNames.size() + " gene names");
+			
+			
+			for ( GeneName g : geneNames){
+				if ( g.getApprovedSymbol().equals("FOLH1"))
+					System.out.println(g);
+			}
+			// and returns a list of beans that contains key-value pairs for each gene name
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	public static List<GeneName> getGeneNames() throws IOException{
+		URL url = new URL(DEFAULT_GENENAMES_URL);
+		
+		InputStreamProvider prov = new InputStreamProvider();
+		
+		InputStream inStream = prov.getInputStream(url);
+				
+		return getGeneNames(inStream);
+	}
+	
+	/** Get a list of GeneNames from an input stream.
+	 * 
+	 * @param inStream
+	 * @return list of geneNames
+	 * @throws IOException
+	 */
+	public static List<GeneName> getGeneNames(InputStream inStream) throws IOException{
+		
+		ArrayList<GeneName> geneNames = new ArrayList<GeneName>();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
+		
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			// process line...
+			//System.out.println(Arrays.toString(line.split("\t")));
+			
+			GeneName  geneName = getGeneName(line);
+			if ( geneName != null)
+				geneNames.add(geneName);
+				//System.out.println(geneName);
+			
+		}
+		
+		// since this is a large list, let's free up unused space...
+		geneNames.trimToSize();
+		return geneNames;
+	}
+
+	private static GeneName getGeneName(String line) {
+		// data is in this order:
+		//[HGNC ID, Approved Symbol, Approved Name, Status, Previous Symbols, 
+		// Previous Names, Synonyms, Chromosome, Accession Numbers, RefSeq IDs, UniProt ID(supplied by UniProt)]
+		
+		if (line == null)
+			return null;
+		
+		String[] s = line.split("\t");
+		
+		if ( s.length != 11) {
+			System.err.println("WARNING line does not contain 11 data items but " + s.length+"."  );
+			System.err.println(line);
+			return null;
+		}
+		GeneName gn = new GeneName();
+		
+		gn.setHgncId(s[0]);
+		gn.setApprovedSymbol(s[1]);
+		gn.setApprovedName(s[2]);
+		gn.setStatus(s[3]);
+		gn.setPreviousSymbols(s[4]);
+		gn.setPreviousNames(s[5]);
+		gn.setSynonyms(s[6]);
+		gn.setChromosome(s[7]);		
+		gn.setAccessionNr(s[8]);
+		gn.setRefseqIds(s[9]);
+		gn.setUniprot(s[10]);
+
+		
+		return gn;
+
+	}
+	
+}
