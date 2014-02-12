fd2bdc0ff6a23a06e8b96228f9d7a76b19452d58
Andreas Prlic <andreas.prlic@gmail.com>
2014-02-12 15:45:20 -0800

    making sure all rows have the same length

--- biojava3-genome/src/main/java/org/biojava3/genome/parsers/genename/GeneNamesParser.java
+++ biojava3-genome/src/main/java/org/biojava3/genome/parsers/genename/GeneNamesParser.java
@@ -40,7 +40,7 @@ import org.biojava3.core.util.InputStreamProvider;
  *
  */
 public class GeneNamesParser {
-	public static final String DEFAULT_GENENAMES_URL = "http://www.genenames.org/cgi-bin/download?title=HGNC+output+data&hgnc_dbtag=on&col=gd_hgnc_id&col=gd_app_sym&col=gd_app_name&col=gd_status&col=gd_prev_sym&col=gd_prev_name&col=gd_aliases&col=gd_pub_chrom_map&col=gd_pub_acc_ids&col=md_mim_id&col=gd_pub_refseq_ids&col=md_ensembl_id&col=md_prot_id" +
+	public static final String DEFAULT_GENENAMES_URL = "http://www.genenames.org/cgi-bin/download?title=HGNC+output+data&hgnc_dbtag=on&col=gd_app_sym&col=gd_app_name&col=gd_status&col=gd_prev_sym&col=gd_prev_name&col=gd_aliases&col=gd_pub_chrom_map&col=gd_pub_acc_ids&col=md_mim_id&col=gd_pub_refseq_ids&col=md_ensembl_id&col=md_prot_id&col=gd_hgnc_id" +
 			 "&status=Approved&status_opt=2&where=((gd_pub_chrom_map%20not%20like%20%27%patch%%27%20and%20gd_pub_chrom_map%20not%20like%20%27%ALT_REF%%27)%20or%20gd_pub_chrom_map%20IS%20NULL)%20and%20gd_locus_group%20%3d%20%27protein-coding%20gene%27&order_by=gd_app_sym_sort&format=text&limit=&submit=submit&.cgifields=&.cgifields=chr&.cgifields=status&.cgifields=hgnc_dbtag";
 
 	/** parses a file from the genenames website 
@@ -122,25 +122,26 @@ public class GeneNamesParser {
 		
 		if ( s.length != 13) {
 			System.err.println("WARNING line does not contain 13 data items but " + s.length+"."  );
+			System.err.println(line);
 			System.err.println(line.replaceAll("\t", "|---|"));
 			return null;
 		}
 		GeneName gn = new GeneName();
 		
-		gn.setHgncId(s[0]);
-		gn.setApprovedSymbol(s[1]);
-		gn.setApprovedName(s[2]);
-		gn.setStatus(s[3]);
-		gn.setPreviousSymbols(s[4]);
-		gn.setPreviousNames(s[5]);
-		gn.setSynonyms(s[6]);
-		gn.setChromosome(s[7]);		
-		gn.setAccessionNr(s[8]);
-		gn.setOmimId(s[9]);
-		gn.setRefseqIds(s[10]);
-		gn.setEnsemblGeneId(s[11]);
-		gn.setUniprot(s[12]);
 		
+		gn.setApprovedSymbol(s[0]);
+		gn.setApprovedName(s[1]);
+		gn.setStatus(s[2]);
+		gn.setPreviousSymbols(s[3]);
+		gn.setPreviousNames(s[4]);
+		gn.setSynonyms(s[5]);
+		gn.setChromosome(s[6]);		
+		gn.setAccessionNr(s[7]);
+		gn.setOmimId(s[8]);
+		gn.setRefseqIds(s[9]);
+		gn.setEnsemblGeneId(s[10]);
+		gn.setUniprot(s[11]);
+		gn.setHgncId(s[12]);
 		
 		return gn;
 
