953af07a92c61a840426fefeeb0e45f627d652b2
pwrose <pwrose@ucsd.edu>
2014-10-06 11:53:54 -0700

    cleanup

--- biojava3-structure/src/main/java/org/biojava/bio/structure/align/FarmJob.java
+++ biojava3-structure/src/main/java/org/biojava/bio/structure/align/FarmJob.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.biojava.bio.structure.align.ce.AbstractUserArgumentProcessor;
 import org.biojava.bio.structure.align.client.FarmJobParameters;
 import org.biojava.bio.structure.align.client.FarmJobRunnable;
 import org.biojava.bio.structure.align.events.AlignmentProgressListener;
