286e7fe72cda604fa8a648edfc7c0247a68f1234
lafita <aleixlafita@gmail.com>
2015-05-20 10:37:38 +0200

    Fix bug in replaceOptAln block calculation

--- biojava-structure/src/main/java/org/biojava/nbio/structure/align/util/AlignmentTools.java
+++ biojava-structure/src/main/java/org/biojava/nbio/structure/align/util/AlignmentTools.java
@@ -909,11 +909,9 @@ public class AlignmentTools {
 		double[] blockRMSD = new double[afpChain.getBlockNum()];
 		double[] blockScore = new double[afpChain.getBlockNum()];
 		for (int k=0; k<afpChain.getBlockNum(); k++){
-			//Clone the AFP for each block calculation
-			AFPChain afpChainb = (AFPChain) afpChain.clone();
 			//Create the atom arrays corresponding to the aligned residues in the block
-			Atom[] ca1block = new Atom[afpChainb.getOptLen()[k]];
-			Atom[] ca2block = new Atom[afpChainb.getOptLen()[k]];
+			Atom[] ca1block = new Atom[afpChain.getOptLen()[k]];
+			Atom[] ca2block = new Atom[afpChain.getOptLen()[k]];
 			int position=0;
 			for(int i=0;i<blockLens[k];i++) {
 				int pos1 = optAln[k][0][i];
@@ -924,8 +922,8 @@ public class AlignmentTools {
 				ca2block[position] = a2;
 				position++;
 			}
-			if (position != afpChainb.getOptLen()[k]){
-				logger.warn("AFPChainScorer getTMScore: Problems reconstructing alignment! nr of loaded atoms is " + pos + " but should be " + afpChainb.getOptLen()[k]);
+			if (position != afpChain.getOptLen()[k]){
+				logger.warn("AFPChainScorer getTMScore: Problems reconstructing alignment! nr of loaded atoms is " + pos + " but should be " + afpChain.getOptLen()[k]);
 				// we need to resize the array, because we allocated too many atoms earlier on.
 				ca1block = (Atom[]) resizeArray(ca1block, position);
 				ca2block = (Atom[]) resizeArray(ca2block, position);
@@ -934,24 +932,19 @@ public class AlignmentTools {
 			SVDSuperimposer svdb = new SVDSuperimposer(ca1block, ca2block);
 			Matrix matrixb = svdb.getRotation();
 			Atom shiftb = svdb.getTranslation();
-			Matrix[] blockMxsb = new Matrix[afpChain.getBlockNum()];
-			Arrays.fill(blockMxsb, matrix);
-			afpChainb.setBlockRotationMatrix(blockMxsb);
-			Atom[] blockShiftsb = new Atom[afpChainb.getBlockNum()];
-			Arrays.fill(blockShiftsb, shiftb);
-			afpChainb.setBlockShiftVector(blockShiftsb);
-
+			Arrays.fill(blockMxs, matrixb);
+			afpChain.setBlockRotationMatrix(blockMxs);
+			Arrays.fill(blockShifts, shiftb);
+			afpChain.setBlockShiftVector(blockShifts);
 			for (Atom a : ca2block) {
 				Calc.rotate(a, matrixb);
 				Calc.shift(a, shiftb);
 			}
-			
 			//Calculate the RMSD and TM score for the block
 			double rmsdb = SVDSuperimposer.getRMS(ca1block, ca2block);
 			double tmScoreb = SVDSuperimposer.getTMScore(ca1block, ca2block, ca1.length, ca2.length);
 			blockRMSD[k] = rmsdb;
 			blockScore[k] = tmScoreb;
-		
 		}
 		afpChain.setOptRmsd(blockRMSD);
 		afpChain.setBlockRmsd(blockRMSD);
