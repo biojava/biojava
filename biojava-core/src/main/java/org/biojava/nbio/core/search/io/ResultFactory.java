051c6c9b0ba68fa075aa078b047e80863883cc44
Paolo Pavan <paolo.pavan@gmail.com>
2015-07-21 13:07:30 +0200

    Updated interface to allow also Results persistence (storing).

--- biojava-core/src/main/java/org/biojava/nbio/core/search/io/ResultFactory.java
+++ biojava-core/src/main/java/org/biojava/nbio/core/search/io/ResultFactory.java
@@ -13,9 +13,30 @@ import java.util.List;
  * @author pavanpa
  */
 public interface ResultFactory {
+    /**
+     * returns a list of file extensions associated to this ResultFactory
+     * 
+     * @return 
+     */
     List<String> getFileExtensions();
     void setFile(File f);
+    /**
+     * Launch the parsing and get back a list of Result objects representing the
+     * search result in the specified file.
+     * 
+     * @param maxEScore
+     * @return
+     * @throws Exception 
+     */
     List<Result> createObjects(double maxEScore) throws Exception;
+    /**
+     * The factory that implements this method will be able to save the Search results
+     * to a file in the same format that it is able to read.
+     * 
+     * @param results
+     * @throws Exception 
+     */
+    void storeObjects(List<Result> results) throws Exception;
     
     /**
      * Specify the collection of sequences objects used as queries in the Search run. 
