66cfcdcc2e013e4c9d279656bb59dc04ea9afffe
Andreas Prlic <andreas.prlic@gmail.com>
2015-07-31 10:03:47 -0700

    #308 adding a test that demonstrates that fetching mmCIF files from remote URLs is broken. Also tests for correct parsing of compressed files.

---
+++ biojava-structure/src/test/java/org/biojava/nbio/structure/io/TestURLBasedFileParsing.java
@@ -0,0 +1,31 @@
+package org.biojava.nbio.structure.io;
+
+import junit.framework.TestCase;
+import org.biojava.nbio.structure.Structure;
+import org.biojava.nbio.structure.StructureException;
+import org.biojava.nbio.structure.StructureIO;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * Created by ap3 on 31/07/2015.
+ */
+public class TestURLBasedFileParsing extends TestCase{
+
+    @Test
+    public void testMMcifURL(){
+
+        String u = "http://ftp.wwpdb.org/pub/pdb/data/biounit/mmCIF/divided/nw/4nwr-assembly1.cif.gz";
+
+        try {
+            Structure s = StructureIO.getStructure(u);
+
+            System.out.println(s);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
