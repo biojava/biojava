dbe75e46265ba439971c66c4b9f22fd737cb2c28
Spencer Bliven <sbliven@ucsd.edu>
2015-12-01 16:01:40 +0100

    Adding biojava license statements

--- biojava-structure/src/main/java/org/biojava/nbio/structure/secstruc/SecStrucPred.java
+++ biojava-structure/src/main/java/org/biojava/nbio/structure/secstruc/SecStrucPred.java
@@ -1,3 +1,23 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ */
 package org.biojava.nbio.structure.secstruc;
 
 import org.biojava.nbio.structure.*;
