f8f130d0857fed2a3f17e30f109cac0c40c3efe6
lafita <aleixlafita@gmail.com>
2016-01-25 17:43:49 +0100

    Update Tests for CeSymm static

--- biojava-structure/src/test/java/org/biojava/nbio/structure/symmetry/internal/TestCeSymm.java
+++ biojava-structure/src/test/java/org/biojava/nbio/structure/symmetry/internal/TestCeSymm.java
@@ -40,20 +40,19 @@ public class TestCeSymm {
 	public void testIndependence() throws IOException, StructureException {
 
 		// Only instantiate one CeSymm class
-		CeSymm ce = new CeSymm();
 		String name;
 		Atom[] atoms;
 
 		name = "1MER.A";
 		atoms = StructureTools.getRepresentativeAtomArray(StructureTools
 				.getStructure(name));
-		ce.analyze(atoms);
+		CeSymm.analyze(atoms);
 
 		name = "1ijq.A:377-641";
 		atoms = StructureTools.getRepresentativeAtomArray(StructureTools
 				.getStructure(name));
 		// This was causing an assertion error if runs are dependent
-		ce.analyze(atoms);
+		CeSymm.analyze(atoms);
 	}
 
 }
