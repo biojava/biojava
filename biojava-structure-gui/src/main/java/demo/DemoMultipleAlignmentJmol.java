13abec3c00ef4a6cab2f672075f4cdb28dedda37
Andreas Prlic <andreas.prlic@gmail.com>
2016-01-25 16:07:34 -0800

    #383 this unit test references code from the testing framework. Disabling for now.

--- biojava-structure-gui/src/main/java/demo/DemoMultipleAlignmentJmol.java
+++ biojava-structure-gui/src/main/java/demo/DemoMultipleAlignmentJmol.java
@@ -24,7 +24,7 @@ import java.io.IOException;
 import org.biojava.nbio.structure.StructureException;
 import org.biojava.nbio.structure.align.gui.MultipleAlignmentDisplay;
 import org.biojava.nbio.structure.align.multiple.MultipleAlignment;
-import org.biojava.nbio.structure.align.multiple.TestSampleGenerator;
+//import org.biojava.nbio.structure.align.multiple.TestSampleGenerator;
 
 /**
  * Demo for visualizing the results of a Multiple Alignment, 
@@ -39,9 +39,12 @@ public class DemoMultipleAlignmentJmol {
 			throws StructureException, IOException{
 		
 		//Obtain a sample MultipleAlignment from the tests
-		MultipleAlignment msa = TestSampleGenerator.testAlignment2();
+
+		//TODO: this references a class from the testing section, needs to be fixed
+
+		//MultipleAlignment msa = TestSampleGenerator.testAlignment2();
 		
 		//Displaying a MultipleAlignment is a one line code
-		MultipleAlignmentDisplay.display(msa);
+		//MultipleAlignmentDisplay.display(msa);
 	}
 }
