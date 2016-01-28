7a9103759c74a01a068654a44bd04234b3ca003a
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2016-01-28 15:16:24 -0800

    Fixing #392

--- biojava-structure/src/main/java/org/biojava/nbio/structure/io/ChargeAdder.java
+++ biojava-structure/src/main/java/org/biojava/nbio/structure/io/ChargeAdder.java
@@ -31,10 +31,14 @@ import org.biojava.nbio.structure.Structure;
 import org.biojava.nbio.structure.io.mmcif.ChemCompGroupFactory;
 import org.biojava.nbio.structure.io.mmcif.model.ChemComp;
 import org.biojava.nbio.structure.io.mmcif.model.ChemCompAtom;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ChargeAdder {
 
-	private Structure structure = null;
+	private static final Logger logger = LoggerFactory.getLogger(ChargeAdder.class);
+	
+	private Structure structure;
 
 	public ChargeAdder(Structure structure) {
 		this.structure = structure;
@@ -54,8 +58,13 @@ public class ChargeAdder {
 					ChemCompAtom cca = chemAtoms.get(i);
 					Atom a = protAtoms.get(i);
 					// Get the charge 
-					short charge = Short.parseShort(cca.getCharge());
-					a.setCharge(charge);
+					try {
+						short charge = Short.parseShort(cca.getCharge());					
+						a.setCharge(charge);
+					} catch (NumberFormatException e) {
+						logger.info("Could not parse charge for atom {} of {}. Will set its charge to 0", a.getName(), g.getPDBName());
+						a.setCharge((short)0);
+					}
 				}
 			}
 		}
