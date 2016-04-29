4200a37af9d52ae773414900778397c618033b02
Anthony Bradley <anthony.richard.bradley@gmail.com>
2016-04-28 18:15:33 -0700

    Added handling of '?' and other non integer string for the charge. Added a test to ensure this is handled appropriately.

--- biojava-structure/src/test/java/org/biojava/nbio/structure/TestChargeAdder.java
+++ biojava-structure/src/test/java/org/biojava/nbio/structure/TestChargeAdder.java
@@ -38,6 +38,34 @@ public class TestChargeAdder {
 			}
 		}
 		// Check that the count is as excpected
-		assertEquals(chargeCount, 425);	
+		assertEquals(425, chargeCount);	
 	}
+	
+	
+	/**
+	 * Test that it can parse '?' values in the CCD.
+	 * @throws StructureException 
+	 * @throws IOException 
+	 */
+	@Test
+	public void testQuestionMark() throws IOException, StructureException {
+		// Get the structure
+		Structure structure = StructureIO.getStructure("3PE6");
+		ChargeAdder.addCharges(structure);
+		// Now count the charges
+		int chargeCount = 0;
+		for(Chain chain : structure.getChains()) {
+			for (Group group : chain.getAtomGroups()) {
+				for (Atom atom : group.getAtoms()) {
+					if (atom.getCharge()!=0) {
+						chargeCount++;
+					}
+				}
+			}
+		}
+		assertEquals(39, chargeCount);
+	}
+	
+	
+	
 }
