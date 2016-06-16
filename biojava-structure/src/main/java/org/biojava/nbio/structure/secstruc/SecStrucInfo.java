cae04bb950d40bb19682a46b5ffa4dbb20906ff3
Mohamed Ezzat <mohamed.ezzat@devfactory.com>
2016-06-16 06:33:57 +0200

    squid:S1206 - "equals(Object obj)" and "hashCode()" should be overridden in pairs

--- biojava-structure/src/main/java/org/biojava/nbio/structure/secstruc/SecStrucInfo.java
+++ biojava-structure/src/main/java/org/biojava/nbio/structure/secstruc/SecStrucInfo.java
@@ -74,6 +74,15 @@ public class SecStrucInfo {
 	}
 
 	@Override
+	public int hashCode() {
+	    final int prime = 31;
+	    int result = 1;
+	    result = prime * result
+		    + ((assignment == null) ? 0 : type.hashCode());
+	    return result;
+	}
+
+	@Override
 	public boolean equals(Object o) {
 		if (!(o instanceof SecStrucInfo))
 			return false;
