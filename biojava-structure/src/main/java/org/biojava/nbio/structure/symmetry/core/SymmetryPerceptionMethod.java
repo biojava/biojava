7603753bd307a97e39815a1120f01c10f3c24953
lafita <aleixlafita@gmail.com>
2016-06-24 11:34:19 +0200

    Create an ENUM for the symmetry perception methods

---
+++ biojava-structure/src/main/java/org/biojava/nbio/structure/symmetry/core/SymmetryPerceptionMethod.java
@@ -0,0 +1,15 @@
+package org.biojava.nbio.structure.symmetry.core;
+
+/**
+ * Method used for symmetry perception in the {@link QuatSymmetryDetector}.
+ * 
+ * @author Aleix Lafita
+ * @since 5.0.0
+ *
+ */
+public enum SymmetryPerceptionMethod {
+	NO_ROTATION,
+	C2_ROTATION,
+	ROTATION,
+	ROTO_TRANSLATION;
+}
