9a27e7fdb3b77970e59eb867487fa64f5ef9c5ee
lafita <aleixlafita@gmail.com>
2016-07-21 15:15:05 +0200

    Simplify the Subunits class

--- biojava-structure/src/main/java/org/biojava/nbio/structure/symmetry/core/Subunits.java
+++ biojava-structure/src/main/java/org/biojava/nbio/structure/symmetry/core/Subunits.java
@@ -22,9 +22,7 @@ package org.biojava.nbio.structure.symmetry.core;
 
 import org.biojava.nbio.structure.Atom;
 import org.biojava.nbio.structure.Calc;
-import org.biojava.nbio.structure.Chain;
 import org.biojava.nbio.structure.cluster.SubunitCluster;
-import org.biojava.nbio.structure.cluster.SubunitClustererMethod;
 import org.biojava.nbio.structure.symmetry.geometry.MomentsOfInertia;
 import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
@@ -36,8 +34,6 @@ import java.util.*;
  * considered for symmetry detection. This class is a helper for the
  * {@link QuatSymmetryDetector} algorithm, since it calculates the
  * {@link MomentsOfInertia} and the centroids of each Subunit.
- * <p>
- * Delete public modifier when finished.
  * 
  * @author Peter Rose
  * @author Aleix Lafita
@@ -48,34 +44,10 @@ public class Subunits {
 	private List<Point3d[]> caCoords = new ArrayList<Point3d[]>();
 	private List<Integer> sequenceClusterIds = new ArrayList<Integer>();
 
-	@Deprecated
-	// Return variable, guessed from clusters
-	private List<Boolean> pseudoStoichiometry = new ArrayList<Boolean>();
-	@Deprecated
-	// Return variable, no longer valid
-	private List<Double> minSequenceIdentity = new ArrayList<Double>();
-	@Deprecated
-	// Return variable, no longer valid
-	private List<Double> maxSequenceIdentity = new ArrayList<Double>();
-
-	@Deprecated
-	// Subunits are not tied to a chain, used for coloring
-	private List<String> chainIds = new ArrayList<String>();
-	@Deprecated
-	// Subunits are not tied to a model? used for coloring
-	private List<Integer> modelNumbers = new ArrayList<Integer>();
-
 	private List<Integer> folds = new ArrayList<Integer>();
 	private List<Point3d> originalCenters = new ArrayList<Point3d>();
 	private List<Point3d> centers = new ArrayList<Point3d>();
 	private List<Vector3d> unitVectors = new ArrayList<Vector3d>();
-
-	@Deprecated
-	// Return variable, should be in the clusters
-	private int nucleicAcidChainCount = 0;
-	@Deprecated
-	// This should be in QuatSymmetryResults
-	private boolean pseudoSymmetric = false;
 
 	private Point3d centroid;
 	private MomentsOfInertia momentsOfInertia = new MomentsOfInertia();
@@ -87,33 +59,14 @@ public class Subunits {
 	 *            CA coordinates of all subunits
 	 * @param sequenceClusterIds
 	 *            ID of the cluster that each subunit belongs to
-	 * @param pseudoStoichiometry
-	 *            Whether pseudosymmetry was used when clustering the subunit
-	 * @param minSequenceIdentity
-	 *            Minimum sequence identity to other cluster members
-	 * @param maxSequenceIdentity
-	 *            Maximum sequence identity to other cluster members
 	 * @param folds
 	 *            Valid symmetry orders for this stoichiometry
-	 * @param chainIds
-	 *            Chain ID for the subunit
-	 * @param modelNumbers
-	 *            Model number for the subunit
 	 */
-	@Deprecated
 	public Subunits(List<Point3d[]> caCoords, List<Integer> sequenceClusterIds,
-			List<Boolean> pseudoStoichiometry,
-			List<Double> minSequenceIdentity, List<Double> maxSequenceIdentity,
-			List<Integer> folds, List<String> chainIds,
-			List<Integer> modelNumbers) {
+			List<Integer> folds) {
 		this.caCoords = caCoords;
 		this.sequenceClusterIds = sequenceClusterIds;
-		this.pseudoStoichiometry = pseudoStoichiometry;
-		this.minSequenceIdentity = minSequenceIdentity;
-		this.maxSequenceIdentity = maxSequenceIdentity;
 		this.folds = folds;
-		this.chainIds = chainIds;
-		this.modelNumbers = modelNumbers;
 	}
 
 	/**
@@ -127,19 +80,8 @@ public class Subunits {
 		// Loop through all subunits in the clusters and fill Lists
 		for (int c = 0; c < clusters.size(); c++) {
 
-			// TODO we should remove these variables
-			minSequenceIdentity.add(0.0);
-			maxSequenceIdentity.add(0.0);
-
-			// Pseudostoichiometry means one structural cluster
-			SubunitClustererMethod method = clusters.get(c)
-					.getClustererMethod();
-			boolean ps = (method == SubunitClustererMethod.STRUCTURE);
-			ps = (ps || method == SubunitClustererMethod.INTERNAL_SYMMETRY);
-
 			for (int s = 0; s < clusters.get(c).size(); s++) {
 				sequenceClusterIds.add(c);
-				pseudoStoichiometry.add(ps);
 
 				Atom[] atoms = clusters.get(c).getAlignedAtomsSubunit(s);
 
@@ -149,20 +91,6 @@ public class Subunits {
 					points[i] = new Point3d(atoms[i].getCoords());
 
 				caCoords.add(points);
-
-				// TODO guess them chain and model (very ugly)
-				Chain chain = atoms[0].getGroup().getChain();
-				String cid = chain.getId();
-				chainIds.add(cid);
-
-				int model = 0;
-				for (int m = 0; m < chain.getStructure().nrModels(); m++) {
-					if (chain.getStructure().getModel(m).contains(chain)) {
-						model = m;
-						break;
-					}
-				}
-				modelNumbers.add(model);
 			}
 		}
 
@@ -189,47 +117,6 @@ public class Subunits {
 
 	public List<Integer> getSequenceClusterIds() {
 		return sequenceClusterIds;
-	}
-
-	public boolean isPseudoStoichiometric() {
-		for (Boolean b : pseudoStoichiometry) {
-			if (b) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public boolean isPseudoSymmetric() {
-		return pseudoSymmetric;
-	}
-
-	public void setPseudoSymmetric(boolean pseudoSymmetric) {
-		this.pseudoSymmetric = pseudoSymmetric;
-	}
-
-	public double getMinSequenceIdentity() {
-		double minId = 1.0;
-		for (double seqId : minSequenceIdentity) {
-			minId = Math.min(seqId, minId);
-		}
-		return minId;
-	}
-
-	public double getMaxSequenceIdentity() {
-		double maxId = 1.0;
-		for (double seqId : maxSequenceIdentity) {
-			maxId = Math.min(seqId, maxId);
-		}
-		return maxId;
-	}
-
-	public List<String> getChainIds() {
-		return chainIds;
-	}
-
-	public List<Integer> getModelNumbers() {
-		return modelNumbers;
 	}
 
 	public List<Integer> getFolds() {
@@ -315,44 +202,6 @@ public class Subunits {
 	public MomentsOfInertia getMomentsOfInertia() {
 		run();
 		return momentsOfInertia;
-	}
-
-	/**
-	 * @return the nucleicAcidChainCount
-	 */
-	public int getNucleicAcidChainCount() {
-		run();
-		return nucleicAcidChainCount;
-	}
-
-	/**
-	 * @param nucleicAcidChainCount
-	 *            the nucleicAcidChainCount to set
-	 */
-	public void setNucleicAcidChainCount(int nucleicAcidChainCount) {
-		this.nucleicAcidChainCount = nucleicAcidChainCount;
-	}
-
-	public boolean overlaps(Subunits subunits) {
-		Set<String> set1 = getSignatures(this);
-		Set<String> set2 = getSignatures(subunits);
-		set1.retainAll(set2);
-		return set1.size() > 0;
-	}
-
-	public boolean contains(Subunits subunits) {
-		Set<String> set1 = getSignatures(this);
-		Set<String> set2 = getSignatures(subunits);
-		return set1.containsAll(set2);
-	}
-
-	private static Set<String> getSignatures(Subunits subunits) {
-		Set<String> set = new HashSet<String>(subunits.getSubunitCount());
-		for (int i = 0; i < subunits.getSubunitCount(); i++) {
-			set.add(subunits.getChainIds().get(i) + "_"
-					+ subunits.getModelNumbers().get(i));
-		}
-		return set;
 	}
 
 	private void run() {
