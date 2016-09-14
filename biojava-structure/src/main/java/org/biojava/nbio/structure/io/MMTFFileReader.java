52c3edb823754750d1854fd436928370b81cb89b
Anthony Bradley <anthony.richard.bradley@gmail.com>
2016-09-14 13:42:19 -0700

    Add constructors for the MMTFFilereader

--- biojava-structure/src/main/java/org/biojava/nbio/structure/io/MMTFFileReader.java
+++ biojava-structure/src/main/java/org/biojava/nbio/structure/io/MMTFFileReader.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.biojava.nbio.structure.Structure;
+import org.biojava.nbio.structure.align.util.UserConfiguration;
 import org.biojava.nbio.structure.io.mmtf.MmtfActions;
 
 /**
@@ -18,7 +19,6 @@ public class MMTFFileReader extends LocalPDBDirectory {
 	public static final String[] MMTF_OBSOLETE_DIR = new String[]{"data","structures","obsolete","mmtf"};
 	
 	public static void main(String[] args) throws Exception {
-
 		MMTFFileReader reader = new MMTFFileReader();
 		FileParsingParameters params = new FileParsingParameters();
 		reader.setFileParsingParameters(params);
@@ -26,6 +26,25 @@ public class MMTFFileReader extends LocalPDBDirectory {
 		System.out.println(struc);
 	}
 	
+	/**
+	 * Constructs a new {@link MMTFFileReader}, initializing the extensions member variable.
+	 * The path is initialized in the same way as {@link UserConfiguration},
+	 * i.e. to system property/environment variable {@link UserConfiguration#PDB_DIR}.
+	 * Both autoFetch and splitDir are initialized to false
+	 */
+	public MMTFFileReader() {
+		this(null);
+	}
+	
+	/**
+	 * Constructs a new {@link MMTFFileReader}, initializing the extensions member variable.
+	 * The path is initialized to the given path, both autoFetch and splitDir are initialized to false.
+	 */
+	public MMTFFileReader(String path) {
+		super(path);
+		addExtension(".mmtf");
+		addExtension(".mmtf.gz");	
+		}
 	
 	@Override
 	public Structure getStructure(InputStream inStream) throws IOException {
