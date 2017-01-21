80a3eaaacaac49c09caf892f56fd5f52858439b0
josemduarte <jose.m.duarte@gmail.com>
2017-01-21 12:31:29 -0800

    Now catching empty arrays in getCentroid

--- biojava-structure/src/main/java/org/biojava/nbio/structure/Calc.java
+++ biojava-structure/src/main/java/org/biojava/nbio/structure/Calc.java
@@ -777,6 +777,10 @@ public class Calc {
 	 * @return an Atom representing the Centroid of the set of atoms
 	 */
 	public static final Atom getCentroid(Atom[] atomSet) {
+		
+		// if we don't catch this case, the centroid returned is (NaN,NaN,NaN), which can cause lots of problems down the line
+		if (atomSet.length==0) 
+			throw new IllegalArgumentException("Atom array has length 0, can't calculate centroid!");
 
 		double[] coords = new double[3];
 
