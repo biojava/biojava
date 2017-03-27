d5024696d8e34264aa61e836b40a27e6f1d028cc
valasatava <yana.valasatava@gmail.com>
2017-03-27 16:02:52 -0700

    .2bit parser facade added

--- biojava-genome/src/main/java/org/biojava/nbio/genome/util/ProteinMappingTools.java
+++ biojava-genome/src/main/java/org/biojava/nbio/genome/util/ProteinMappingTools.java
@@ -2,6 +2,7 @@ package org.biojava.nbio.genome.util;
 
 import org.biojava.nbio.core.exceptions.CompoundNotFoundException;
 import org.biojava.nbio.core.sequence.DNASequence;
+import org.biojava.nbio.core.sequence.ProteinSequence;
 import org.biojava.nbio.core.sequence.RNASequence;
 
 public class ProteinMappingTools {
@@ -12,9 +13,9 @@ public class ProteinMappingTools {
     * 
     * @return the protein sequence
     */
-	public String convertDNAtoProteinSequence(String dnaSequence) throws CompoundNotFoundException {
+	public ProteinSequence convertDNAtoProteinSequence(String dnaSequence) throws CompoundNotFoundException {
 		DNASequence dna = new DNASequence(dnaSequence);
 		RNASequence mRNA = dna.getRNASequence();
-		return mRNA.getProteinSequence().toString();
+		return mRNA.getProteinSequence();
 	}
 }
