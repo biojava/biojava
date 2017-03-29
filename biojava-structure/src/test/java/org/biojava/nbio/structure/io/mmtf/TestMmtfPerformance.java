f39c4cb7ef939816d2a2b91ddacab64d7eed9856
Andreas Prlic <andreas.prlic@gmail.com>
2017-03-29 14:20:52 -0700

    new version of test now only compares parsing in RAM

--- biojava-structure/src/test/java/org/biojava/nbio/structure/io/mmtf/TestMmtfPerformance.java
+++ biojava-structure/src/test/java/org/biojava/nbio/structure/io/mmtf/TestMmtfPerformance.java
@@ -2,6 +2,7 @@ package org.biojava.nbio.structure.io.mmtf;
 
 import org.biojava.nbio.structure.Structure;
 import org.biojava.nbio.structure.StructureIO;
+import org.biojava.nbio.structure.TestStructureCrossReferences;
 import org.biojava.nbio.structure.io.PDBFileParser;
 import org.biojava.nbio.structure.io.mmcif.AllChemCompProvider;
 import org.biojava.nbio.structure.io.mmcif.ChemCompGroupFactory;
@@ -9,21 +10,26 @@ import org.biojava.nbio.structure.io.mmcif.ChemCompProvider;
 import org.junit.Test;
 import org.rcsb.mmtf.dataholders.MmtfStructure;
 import org.rcsb.mmtf.decoder.ReaderUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.zip.GZIPInputStream;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
  * Created by andreas on 1/9/17.
  */
 public class TestMmtfPerformance {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestMmtfPerformance.class);
+
 
 //    @Test
 //    public void test3J3Q() throws IOException{
@@ -42,35 +48,96 @@ public class TestMmtfPerformance {
 //
 //    }
 
+
+    // Returns the contents of the file in a byte array.
+    public static byte[] getBytesFromFile(File file) throws IOException {
+        // Get the size of the file
+        long length = file.length();
+
+        // You cannot create an array using a long type.
+        // It needs to be an int type.
+        // Before converting to an int type, check
+        // to ensure that file is not larger than Integer.MAX_VALUE.
+        if (length > Integer.MAX_VALUE) {
+            // File is too large
+            throw new IOException("File is too large!");
+        }
+
+        // Create the byte array to hold the data
+        byte[] bytes = new byte[(int)length];
+
+        // Read in the bytes
+        int offset = 0;
+        int numRead = 0;
+
+        InputStream is = new FileInputStream(file);
+        try {
+            while (offset < bytes.length
+                    && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
+                offset += numRead;
+            }
+        } finally {
+            is.close();
+        }
+
+        // Ensure all the bytes have been read in
+        if (offset < bytes.length) {
+            throw new IOException("Could not completely read file "+file.getName());
+        }
+        return bytes;
+    }
+
+    static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+
+
+    /** loads both pdb and mmtf file for 4CUP in memory and compares parsing performance.
+     * That means any IO is excluded from the measurement and we really compare raw parsing speed.
+     *
+     * @throws IOException
+     */
     @Test
     public void test4CUP() throws IOException{
 
-        long timeS = System.currentTimeMillis();
-
         ClassLoader classLoader = getClass().getClassLoader();
-        Structure structure = MmtfActions.readFromFile((Paths.get(classLoader.getResource("org/biojava/nbio/structure/io/mmtf/4CUP.mmtf").getPath())));
 
-        assertEquals(structure.getPDBCode(),"4CUP");
-        assertEquals(structure.getChains().size(),6);
+        Path p = Paths.get(classLoader.getResource("org/biojava/nbio/structure/io/mmtf/4CUP.mmtf").getPath());
+        byte[] mmtfBytes = Files.readAllBytes(p);
 
-        long timeE = System.currentTimeMillis();
+        InputStream mmtfIS = new ByteArrayInputStream(mmtfBytes);
 
-        Path path = Paths.get(classLoader.getResource("org/biojava/nbio/structure/io/4cup.pdb").getPath());
+        long mmtfStart = System.currentTimeMillis();
+        Structure structure = MmtfActions.readFromInputStream(mmtfIS);
+        long mmtfEnd = System.currentTimeMillis();
 
-        InputStream is = Files.newInputStream(path);
+        assertEquals(structure.getPDBCode(), "4CUP");
+        assertEquals(structure.getChains().size(), 6);
 
+
+        ///end of mmtf parsing. Now we parse PDB:
+
+
+        Path path = Paths.get(classLoader.getResource("org/biojava/nbio/structure/io/4cup.pdb.gz").getPath());
+
+        InputStream is = new GZIPInputStream(Files.newInputStream(path));
+
+        String pdbFile = convertStreamToString(is);
+
+        long pdbStart = System.currentTimeMillis();
         PDBFileParser parser = new PDBFileParser();
 
-        Structure s = parser.parsePDBFile(is);
+        Structure s = parser.parsePDBFile(new ByteArrayInputStream(pdbFile.getBytes()));
 
-        long timeF = System.currentTimeMillis();
+        long pdbEnd = System.currentTimeMillis();
 
         //todo: add mmcif for comparison
 
-//        System.out.println("time to parse mmtf:" + (timeE-timeS));
-//        System.out.println("time to parse PDB: " + (timeF-timeE));
+        logger.warn("time to parse mmtf:" + (mmtfEnd-mmtfStart));
+        logger.warn("time to parse PDB: " + (pdbEnd-pdbStart));
 
-        assertTrue( "It should not be the case, but it is faster to parse a PDB file ("+(timeF -timeE)+" ms.) than MMTF ("+( timeE-timeS)+" ms.)!",( timeF -timeE) > ( timeE-timeS));
+        assertTrue( "It should not be the case, but it is faster to parse a PDB file ("+(pdbEnd -pdbStart)+" ms.) than MMTF ("+( mmtfEnd-mmtfStart)+" ms.)!",( pdbEnd -pdbStart) > ( mmtfEnd-mmtfStart));
 
     }
 }
