b81c37d7d547bf63aeac00d887ba8803b91e4869
valasatava <yana.valasatava@gmail.com>
2017-10-06 17:10:32 -0700

    several fixes for biological assemblies calculations

--- biojava-structure/src/main/java/org/biojava/nbio/structure/cluster/SubunitExtractor.java
+++ biojava-structure/src/main/java/org/biojava/nbio/structure/cluster/SubunitExtractor.java
@@ -68,19 +68,15 @@ public class SubunitExtractor {
 		// The extracted subunit container
 		List<Subunit> subunits = new ArrayList<Subunit>();
 
-		
-		for (Chain c : structure.getChains()) {
-
+		for (Chain c : structure.getPolyChains()) {
 			// Only take protein chains
 			if (c.isProtein()) {
 				Atom[] ca = StructureTools.getRepresentativeAtomArray(c);
 				logger.debug("Chain " + c.getId() + "; CA Atoms: "
 						+ ca.length + "; SEQRES: " + c.getSeqResSequence());
 				subunits.add(new Subunit(ca, c.getName(), null, structure));
-
 			}
 		}
-		
 
 		// Calculate the minimum length of a Subunit
 		int adjustedMinLen = calcAdjustedMinimumSequenceLength(subunits,
