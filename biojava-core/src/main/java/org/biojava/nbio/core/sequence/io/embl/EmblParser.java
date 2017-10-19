3dc43b4cfcdc6ba38855fbf50dfacb2e53f35d67
Noor Almbaidin <noor.almbaidin@progressoft.com>
2017-10-19 14:44:55 +0300

    t

--- biojava-core/src/main/java/org/biojava/nbio/core/sequence/io/embl/EmblParser.java
+++ biojava-core/src/main/java/org/biojava/nbio/core/sequence/io/embl/EmblParser.java
@@ -2,18 +2,21 @@ package org.biojava.nbio.core.sequence.io.embl;
 
 
 import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
 
 public class EmblParser {
 
     private File file;
-    private EmblId id;
-    private EmblReference emblReference;
-    private String accessionNumber;
+    private EmblId emblId = new EmblId();
+    private EmblReference emblReference = new EmblReference();
+    private List<String> accessionNumber = new LinkedList<>();
     private String ProjectIdentifier;
+    private String OrGanelle;
     private String createdDate;
     private String lastUpdatedDate;
     private String sequenceDescription;
-    private String keyword;
+    private List<String> keyword = new LinkedList<>();
     private String organismSpecies;
     private String organismClassification;
     private String databaseCrossReference;
@@ -24,125 +27,74 @@ public class EmblParser {
     private StringBuilder sequence;
 
     public EmblId getEmblId() {
-        return id;
+        return emblId;
     }
 
     public EmblReference getEmblReference() {
         return emblReference;
     }
 
-
-    public String getAccessionNumber() {
+    public List<String> getAccessionNumber() {
         return accessionNumber;
-    }
-
-    public void setAccessionNumber(String accessionNumber) {
-        this.accessionNumber = accessionNumber;
     }
 
     public String getProjectIdentifier() {
         return ProjectIdentifier;
     }
 
-    public void setProjectIdentifier(String projectIdentifier) {
-        ProjectIdentifier = projectIdentifier;
-    }
-
     public String getCreatedDate() {
         return createdDate;
-    }
-
-    public void setCreatedDate(String createdDate) {
-        this.createdDate = createdDate;
     }
 
     public String getLastUpdatedDate() {
         return lastUpdatedDate;
     }
 
-    public void setLastUpdatedDate(String lastUpdatedDate) {
-        this.lastUpdatedDate = lastUpdatedDate;
-    }
-
     public String getSequenceDescription() {
         return sequenceDescription;
     }
 
-    public void setSequenceDescription(String sequenceDescription) {
-        this.sequenceDescription = sequenceDescription;
-    }
-
-    public String getKeyword() {
+    public List<String> getKeyword() {
         return keyword;
-    }
-
-    public void setKeyword(String keyword) {
-        this.keyword = keyword;
     }
 
     public String getOrganismSpecies() {
         return organismSpecies;
     }
 
-    public void setOrganismSpecies(String organismSpecies) {
-        this.organismSpecies = organismSpecies;
-    }
-
     public String getOrganismClassification() {
         return organismClassification;
     }
 
-    public void setOrganismClassification(String organismClassification) {
-        this.organismClassification = organismClassification;
-    }
 
     public String getDatabaseCrossReference() {
         return databaseCrossReference;
-    }
-
-    public void setDatabaseCrossReference(String databaseCrossReference) {
-        this.databaseCrossReference = databaseCrossReference;
     }
 
     public String getAssemblyHeader() {
         return assemblyHeader;
     }
 
-    public void setAssemblyHeader(String assemblyHeader) {
-        this.assemblyHeader = assemblyHeader;
-    }
 
     public String getAssemblyInformation() {
         return assemblyInformation;
     }
 
-    public void setAssemblyInformation(String assemblyInformation) {
-        this.assemblyInformation = assemblyInformation;
-    }
 
     public String getCON() {
         return CON;
     }
 
-    public void setCON(String CON) {
-        this.CON = CON;
-    }
 
     public String getSequenceHeader() {
         return sequenceHeader;
     }
 
-    public void setSequenceHeader(String sequenceHeader) {
-        this.sequenceHeader = sequenceHeader;
-    }
 
     public StringBuilder getSequence() {
         return sequence;
     }
 
-    public void setSequence(StringBuilder sequence) {
-        this.sequence = sequence;
-    }
 
     public EmblParser(File file) {
         setFile(file);
@@ -157,11 +109,37 @@ public class EmblParser {
     public void parse() {
         try (FileReader fileReader = new FileReader(file)) {
             String line = "";
+            String lineIdentifier;
+            String lineInfo;
             try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
                 while (bufferedReader.readLine() != null)
                     line = bufferedReader.readLine();
-                if (line.substring(0, 2).equals("ID"))
+                lineInfo = line.substring(2);
+                lineIdentifier = line.substring(0, 2);
+                if (lineIdentifier.equals("ID"))
                     populateID(line);
+                else if (lineIdentifier.equals("AC"))
+                    populateAccessionNumber(line);
+                else if (lineIdentifier.equals("DT") && line.contains("Created"))
+                    createdDate = lineInfo;
+                else if (lineIdentifier.equals("DT") && line.contains("updated"))
+                    lastUpdatedDate = lineInfo;
+                else if (lineIdentifier.equals("DE"))
+                    sequenceDescription = lineInfo;
+                else if (lineIdentifier.equals("KW"))
+                    keyword.add(lineInfo);
+                else if (lineIdentifier.equals("OS"))
+                    organismSpecies = lineInfo;
+                else if (lineIdentifier.equals("OC"))
+                    organismClassification = lineInfo;
+                else if (lineIdentifier.equals("OG"))
+                    OrGanelle = lineInfo;
+                else if (lineIdentifier.equals("RN") || lineIdentifier.equals("RP") || lineIdentifier.equals("RX")
+                        || lineIdentifier.equals("RG") || lineIdentifier.equals("RA")
+                        || lineIdentifier.equals("RT") || lineIdentifier.equals("RL"))
+                    emblReferencePopulating(lineIdentifier, lineInfo);
+
+
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -170,8 +148,37 @@ public class EmblParser {
         }
     }
 
+    private void emblReferencePopulating(String lineIdentifier, String lineInfo) {
+        if (lineIdentifier.equals("RN"))
+            emblReference.setReferenceNumber(lineInfo);
+        else if (lineIdentifier.equals("RP"))
+            emblReference.setReferencePosition(lineInfo);
+        else if (lineIdentifier.equals("RX"))
+            emblReference.setReferenceCrossReference(lineInfo);
+        else if (lineIdentifier.equals("RG"))
+            emblReference.setReferenceGroup(lineInfo);
+        else if (lineIdentifier.equals("RA"))
+            emblReference.setReferenceAuthor(lineInfo);
+        else if (lineIdentifier.equals("RT"))
+            emblReference.setReferenceTitle(lineInfo);
+        else if (lineIdentifier.equals("RL"))
+            emblReference.setReferenceLocation(lineInfo);
+    }
+
+    private void populateAccessionNumber(String line) {
+        accessionNumber.add(line);
+    }
+
     private void populateID(String line) {
-        
+        line.replace(",", "");
+        String[] strings = line.split(" ");
+        emblId.setPrimaryAccession(strings[1]);
+        emblId.setSequenceVersion(strings[2]);
+        emblId.setTopology(strings[3]);
+        emblId.setMoleculeType(strings[4]);
+        emblId.setDataClass(strings[5]);
+        emblId.setTaxonomicDivision(strings[6]);
+        emblId.setSequenceLength(strings[7]);
     }
 
 
