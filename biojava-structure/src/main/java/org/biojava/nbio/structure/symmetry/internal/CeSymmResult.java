815c3487d7e863b734e1a7f1fa9092a31d2cd13f
Spencer Bliven <spencer.bliven@gmail.com>
2018-01-12 22:36:27 +0100

    Fix rcsb/symmetry#92: NullPointerException

--- biojava-structure/src/main/java/org/biojava/nbio/structure/symmetry/internal/CeSymmResult.java
+++ biojava-structure/src/main/java/org/biojava/nbio/structure/symmetry/internal/CeSymmResult.java
@@ -81,7 +81,7 @@ public class CeSymmResult {
 		// If the refinement was attempted
 		if (params.getRefineMethod() != RefineMethod.NOT_REFINED) {
 			// Check for minimum length
-			if (multipleAlignment.getCoreLength() < params.getMinCoreLength())
+			if (multipleAlignment == null || multipleAlignment.getCoreLength() < params.getMinCoreLength())
 				return false;
 			// Allow 90% of original TM-Score theshold
 			if (multipleAlignment.getScore(MultipleAlignmentScorer.AVGTM_SCORE) < params
