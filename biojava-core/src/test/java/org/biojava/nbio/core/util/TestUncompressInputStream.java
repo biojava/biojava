494110294d58fabda20d06a9a26d221efebe152f
Fred Hansen <zweibieren@yahoo.com>
2018-01-25 15:52:04 -0500

    add bigger test; fixed the little test

--- biojava-core/src/test/java/org/biojava/nbio/core/util/TestUncompressInputStream.java
+++ biojava-core/src/test/java/org/biojava/nbio/core/util/TestUncompressInputStream.java
@@ -1,10 +1,12 @@
 package org.biojava.nbio.core.util;
 
+import java.io.BufferedInputStream;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import org.junit.Assert;
 
 public class TestUncompressInputStream {
 	
@@ -16,19 +18,35 @@ public class TestUncompressInputStream {
 	 * The original compress_test.txt contains text {@value #TEXT_IN_FILE} 
 	 */
 	private static final String TEST_FILE = "org/biojava/nbio/core/util/compress_test.txt.lzc";
-	private static final String TEXT_IN_FILE = "Test of biojava uncompress.";
+	private static final String TEXT_IN_FILE = "Test of biojava uncompress.\n";
 	
+	private static final String BIGGER_TEST_FILE = "org/biojava/nbio/core/util/build-copy.xml.Z";
+	private static final String ORIG_OF_BIGGER_TEST_FILE = "org/biojava/nbio/core/util/build.xml";
+        
 	@Test
 	public void testUncompression() throws Exception {
 		
 		InputStream is = this.getClass().getClassLoader().getResourceAsStream(TEST_FILE);
-		
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		UncompressInputStream.uncompress(is, baos);
 		String decompressedText = baos.toString();
 		
 		assertEquals(TEXT_IN_FILE, decompressedText);
-		
-	}
 
+		is = this.getClass().getClassLoader().getResourceAsStream(BIGGER_TEST_FILE);
+		baos = new ByteArrayOutputStream();
+		UncompressInputStream.uncompress(is, baos);
+
+                ByteArrayOutputStream obaos = new ByteArrayOutputStream();
+                try (BufferedInputStream oin = new BufferedInputStream(
+                            this.getClass().getClassLoader()
+                                .getResourceAsStream(ORIG_OF_BIGGER_TEST_FILE));) {
+                    byte[] buf = new byte[100000];
+                    int len;
+                    while ((len = oin.read(buf)) >= 0)
+                        obaos.write(buf, 0, len);
+                }
+                
+                Assert.assertArrayEquals(baos.toByteArray(), obaos.toByteArray());
+	}
 }
