8905560daa608bdfea642e7b919f92160bb23db3
Jose Duarte <jose.m.duarte@gmail.com>
2018-03-11 18:59:25 -0700

    Adding header file and modified addlicense script to read from it

--- development/addlicense.sh
+++ development/addlicense.sh
@@ -1,32 +1,13 @@
 #!/bin/bash
 # Adds the BioJava LGPL license statement to the top of every java file
 
+BASEDIR=$(dirname "$0")
+
 find . -iname '*.java' -exec grep -L 'http://www.gnu.org/copyleft/lesser.html' '{}' '+'|
 xargs grep -Li 'copyright' |
 while read file; do
     echo "$file"
-    cat >tmp.java <<END
-/*
- *                    BioJava development code
- *
- * This code may be freely distributed and modified under the
- * terms of the GNU Lesser General Public Licence.  This should
- * be distributed with the code.  If you do not have a copy,
- * see:
- *
- *      http://www.gnu.org/copyleft/lesser.html
- *
- * Copyright for this code is held jointly by the individual
- * authors.  These should be listed in @author doc comments.
- *
- * For more information on the BioJava project and its aims,
- * or to join the biojava-l mailing list, visit the home page
- * at:
- *
- *      http://www.biojava.org/
- *
- */
-END
+    cat $BASEDIR/../HEADER.txt > tmp.java
     cat "$file" >> tmp.java
     mv tmp.java "$file"
 done
