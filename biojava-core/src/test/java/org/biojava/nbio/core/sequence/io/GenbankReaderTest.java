bb0d3fe166313064c221f637afe40d55fbaa7753
James Morris <jamesanthonymorris@gmail.com>
2019-04-21 22:53:25 +0100

    Update GenbankReaderTest.java
    Added imports for InputStreamReader and BufferedReader

--- biojava-core/src/test/java/org/biojava/nbio/core/sequence/io/GenbankReaderTest.java
+++ biojava-core/src/test/java/org/biojava/nbio/core/sequence/io/GenbankReaderTest.java
@@ -42,8 +42,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
