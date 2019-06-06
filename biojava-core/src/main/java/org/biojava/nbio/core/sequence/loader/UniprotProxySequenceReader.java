390e6b39e87088e356e33bf00cb32459ce970523
emckee2006 <emckee2006@gmail.com>
2019-06-06 15:26:39 -0400

    Update UniprotProxySequenceReader.java
    Parse protein names more thouroughly. This should hit everything mentioned in the current XSD

--- biojava-core/src/main/java/org/biojava/nbio/core/sequence/loader/UniprotProxySequenceReader.java
+++ biojava-core/src/main/java/org/biojava/nbio/core/sequence/loader/UniprotProxySequenceReader.java
@@ -381,39 +381,94 @@ public class UniprotProxySequenceReader<C extends Compound> implements ProxySequ
 		Element uniprotElement = uniprotDoc.getDocumentElement();
 		Element entryElement = XMLHelper.selectSingleElement(uniprotElement, "entry");
 		Element proteinElement = XMLHelper.selectSingleElement(entryElement, "protein");
-		ArrayList<Element> keyWordElementList = XMLHelper.selectElements(proteinElement, "alternativeName");
+		
+		ArrayList<Element> keyWordElementList;
+		getProteinAliasesFromNameGroup(aliasList, proteinElement);
+		
+		keyWordElementList = XMLHelper.selectElements(proteinElement, "component");
 		for (Element element : keyWordElementList) {
-			Element fullNameElement = XMLHelper.selectSingleElement(element, "fullName");
-			aliasList.add(fullNameElement.getTextContent());
-			Element shortNameElement = XMLHelper.selectSingleElement(element, "shortName");
-			if(null != shortNameElement) {
-				String shortName = shortNameElement.getTextContent();
-				if(null != shortName && !shortName.trim().isEmpty()) {
-					aliasList.add(shortName);
-				}
+			getProteinAliasesFromNameGroup(aliasList, element);
+		}
+
+		keyWordElementList = XMLHelper.selectElements(proteinElement, "domain");
+		for (Element element : keyWordElementList) {
+			getProteinAliasesFromNameGroup(aliasList, element);
+		}
+
+		keyWordElementList = XMLHelper.selectElements(proteinElement, "submittedName");
+		for (Element element : keyWordElementList) {
+			getProteinAliasesFromNameGroup(aliasList, element);
+		}
+
+		keyWordElementList = XMLHelper.selectElements(proteinElement, "cdAntigenName");
+		for (Element element : keyWordElementList) {
+			String cdAntigenName = element.getTextContent();
+			if(null != cdAntigenName && !cdAntigenName.trim().isEmpty()) {
+				aliasList.add(cdAntigenName);
 			}
 		}
-		keyWordElementList = XMLHelper.selectElements(proteinElement, "recommendedName");
+			
+		keyWordElementList = XMLHelper.selectElements(proteinElement, "innName");
 		for (Element element : keyWordElementList) {
-			Element fullNameElement = XMLHelper.selectSingleElement(element, "fullName");
-			aliasList.add(fullNameElement.getTextContent());
-			Element shortNameElement = XMLHelper.selectSingleElement(element, "shortName");
-			if(null != shortNameElement) {
-				String shortName = shortNameElement.getTextContent();
-				if(null != shortName && !shortName.trim().isEmpty()) {
-					aliasList.add(shortName);
-				}
+			String cdAntigenName = element.getTextContent();
+			if(null != cdAntigenName && !cdAntigenName.trim().isEmpty()) {
+				aliasList.add(cdAntigenName);
 			}
 		}
-		Element cdAntigen = XMLHelper.selectSingleElement(proteinElement, "cdAntigenName");
-		if(null != cdAntigen) {
-			String cdAntigenName = cdAntigen.getTextContent();
+
+		keyWordElementList = XMLHelper.selectElements(proteinElement, "biotechName");
+		for (Element element : keyWordElementList) {
+			String cdAntigenName = element.getTextContent();
+			if(null != cdAntigenName && !cdAntigenName.trim().isEmpty()) {
+				aliasList.add(cdAntigenName);
+			}
+		}
+
+		keyWordElementList = XMLHelper.selectElements(proteinElement, "allergenName");
+		for (Element element : keyWordElementList) {
+			String cdAntigenName = element.getTextContent();
 			if(null != cdAntigenName && !cdAntigenName.trim().isEmpty()) {
 				aliasList.add(cdAntigenName);
 			}
 		}
 
 		return aliasList;
+	}
+
+	/**
+	 * @param aliasList
+	 * @param proteinElement
+	 * @throws XPathExpressionException
+	 */
+	private void getProteinAliasesFromNameGroup(ArrayList<String> aliasList, Element proteinElement)
+			throws XPathExpressionException {
+		ArrayList<Element> keyWordElementList = XMLHelper.selectElements(proteinElement, "alternativeName");
+		for (Element element : keyWordElementList) {
+			getProteinAliasesFromElement(aliasList, element);
+		}
+		
+		keyWordElementList = XMLHelper.selectElements(proteinElement, "recommendedName");
+		for (Element element : keyWordElementList) {
+			getProteinAliasesFromElement(aliasList, element);
+		}
+	}
+
+	/**
+	 * @param aliasList
+	 * @param element
+	 * @throws XPathExpressionException
+	 */
+	private void getProteinAliasesFromElement(ArrayList<String> aliasList, Element element)
+			throws XPathExpressionException {
+		Element fullNameElement = XMLHelper.selectSingleElement(element, "fullName");
+		aliasList.add(fullNameElement.getTextContent());
+		Element shortNameElement = XMLHelper.selectSingleElement(element, "shortName");
+		if(null != shortNameElement) {
+			String shortName = shortNameElement.getTextContent();
+			if(null != shortName && !shortName.trim().isEmpty()) {
+				aliasList.add(shortName);
+			}
+		}
 	}
 
 	/**
