ee6a478bbf36bdd1e82015a0500b8eb2d83658d1
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2019-09-02 16:54:56 -0700

    New test for #792

--- biojava-structure/src/test/java/org/biojava/nbio/structure/io/mmtf/TestMmtfStructureReader.java
+++ biojava-structure/src/test/java/org/biojava/nbio/structure/io/mmtf/TestMmtfStructureReader.java
@@ -4,11 +4,19 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.List;
+
+import org.biojava.nbio.structure.Group;
 import org.biojava.nbio.structure.Structure;
 import org.biojava.nbio.structure.StructureException;
 import org.biojava.nbio.structure.StructureIO;
+import org.biojava.nbio.structure.align.util.AtomCache;
+import org.biojava.nbio.structure.io.FileParsingParameters;
+import org.biojava.nbio.structure.io.mmcif.ChemCompGroupFactory;
+import org.biojava.nbio.structure.io.mmcif.DownloadChemCompProvider;
 import org.junit.Ignore;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Test the Biojava MMTF reader.
@@ -69,5 +77,32 @@ public class TestMmtfStructureReader {
 				mmtf.getChainByIndex(0).getSeqResSequence());
 		
 	}
+
+	/**
+	 * Test for issue https://github.com/biojava/biojava/issues/792
+	 */
+	@Test
+	@Ignore("Issue not fixed yet")
+	public void checkNonStandardAminoSeqresGroupsPopulated() throws StructureException, IOException {
+	    // 2X3T, see issue https://github.com/biojava/biojava/issues/792
+        // Load a structure in mmtf format
+        AtomCache cache = new AtomCache();
+        FileParsingParameters params = new FileParsingParameters();
+        cache.setFileParsingParams(params);
+        cache.setUseMmCif(false);
+        cache.setUseMmtf(true);
+
+        StructureIO.setAtomCache(cache);
+
+        ChemCompGroupFactory.setChemCompProvider(new DownloadChemCompProvider());
+
+        Structure structure1 = StructureIO.getStructure("2X3T");
+        // chain E is a glycopeptide with unobserved non-standard aminoacids. Because of mmtf limitations (representing seqres sequences as 1-letter strings) the non-standard unobserved residues are read as null
+        List<Group> seqresGroups = structure1.getChain("E").getSeqResGroups();
+        for (Group g : seqresGroups) {
+            assertNotNull("SeqRes group should not be null", g);
+        }
+
+    }
 
 }
