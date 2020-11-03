7c888429f5eb0e76def176f87eeb4ac2c992c2cb
Jose Duarte <jose.m.duarte@gmail.com>
2020-11-02 16:39:29 -0800

    Fixing potential bugs

--- biojava-structure/src/main/java/org/biojava/nbio/structure/secstruc/DSSPParser.java
+++ biojava-structure/src/main/java/org/biojava/nbio/structure/secstruc/DSSPParser.java
@@ -155,16 +155,16 @@ public class DSSPParser {
 
 				//Parse the Bridge partners - TODO parallel or antiparallel?
 				String bp = line.substring(25,29).trim();
-				if (bp != "") {
+				if (!bp.equals("")) {
 					BetaBridge bb = new BetaBridge(
-							index, Integer.valueOf(bp), BridgeType.parallel);
+							index, Integer.parseInt(bp), BridgeType.parallel);
 					ss.addBridge(bb);
 				} else logger.warn("Unable to parse beta Bridge for resn "+index);
 
 				bp = line.substring(29,33).trim();
-				if (bp != "") {
+				if (!bp.equals("")) {
 					BetaBridge bb = new BetaBridge(
-							index, Integer.valueOf(bp), BridgeType.parallel);
+							index, Integer.parseInt(bp), BridgeType.parallel);
 					ss.addBridge(bb);
 				} else logger.warn("Unable to parse beta Bridge for resn "+index);
 
@@ -175,7 +175,7 @@ public class DSSPParser {
 					int b = a + 8;
 
 					String val = line.substring(a,b).trim();
-					if (val == "") {
+					if (val.equals("")) {
 						logger.warn("Unable to parse energy for resn "+index);
 						continue;
 					}
@@ -184,7 +184,7 @@ public class DSSPParser {
 
 					int partner = Integer.parseInt(p[0]);
 					if (partner != 0) partner += index;
-					double energy = Double.valueOf(p[1]) * 1000.0;
+					double energy = Double.parseDouble(p[1]) * 1000.0;
 
 					switch(i){
 					case 0:
@@ -208,15 +208,15 @@ public class DSSPParser {
 
 				//Angle properties
 				String val = line.substring(91,97).trim();
-				if (val != "") ss.setKappa(Float.valueOf(val));
+				if (!val.equals("")) ss.setKappa(Float.parseFloat(val));
 				else logger.warn("Unable to parse kappa for resn "+index);
 
 				val = line.substring(103,109).trim();
-				if (val != "") ss.setPhi(Float.valueOf(val));
+				if (!val.equals("")) ss.setPhi(Float.parseFloat(val));
 				else logger.warn("Unable to parse phi for resn "+index);
 
 				val = line.substring(109,116).trim();
-				if (val != "") ss.setPsi(Float.valueOf(val));
+				if (!val.equals("")) ss.setPsi(Float.parseFloat(val));
 				else logger.warn("Unable to parse psi for resn "+index);
 
 				if (assign) parent.setProperty(Group.SEC_STRUC, ss);
