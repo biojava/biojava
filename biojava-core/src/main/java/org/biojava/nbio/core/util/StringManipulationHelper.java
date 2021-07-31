e7ca188348332da5a80afe2c582d8405dcdca47d
Richard Adams <ra22597@gmail.com>
2021-07-31 11:04:08 +0100

    remove unused import

--- biojava-core/src/main/java/org/biojava/nbio/core/util/StringManipulationHelper.java
+++ biojava-core/src/main/java/org/biojava/nbio/core/util/StringManipulationHelper.java
@@ -29,7 +29,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
