195d3da3c7295ca6bd77f77602f5b383ae35bbc0
richard <ra22597@gmail.com>
2021-11-14 22:55:40 +0000

    update InsdcParserTest to use jupiter junit5 parameterized test

--- biojava-core/src/test/java/org/biojava/nbio/core/sequence/location/InsdcParserTest.java
+++ biojava-core/src/test/java/org/biojava/nbio/core/sequence/location/InsdcParserTest.java
@@ -20,6 +20,7 @@
  */
 package org.biojava.nbio.core.sequence.location;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.Arrays;
@@ -27,10 +28,9 @@ import java.util.Collection;
 import org.biojava.nbio.core.sequence.DataSource;
 import org.biojava.nbio.core.sequence.location.InsdcParser.complexFeaturesAppendEnum;
 import org.biojava.nbio.core.sequence.location.template.Location;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,23 +38,14 @@ import org.slf4j.LoggerFactory;
  *
  * @author Jacek Grzebyta
  */
-@RunWith(Parameterized.class)
+
 public class InsdcParserTest {
 
 	private Logger log = LoggerFactory.getLogger(getClass());
-	private String data;
-	private String expected;
 
-	public InsdcParserTest(String data, String expected) {
-		this.data = data;
-		this.expected = expected;
-	}
+	static Collection<String[]> data() {
+		return Arrays.asList(new String[][] {
 
-	@Parameterized.Parameters
-	public static Collection<String[]> data() {
-		return Arrays.asList(new String[][]{
-			{"complement(CP001663.1:6463934..6465826)", "CP001663.1"},
-			{"complement(NC_000932.1:69611..69724)", "NC_000932.1"}
 		});
 	}
 
@@ -63,10 +54,10 @@ public class InsdcParserTest {
 	 *
 	 * @throws Exception
 	 */
-	@Test
-	public void extractAccessionTest() throws Exception {
-		log.info("test accession");
-		log.debug("data: '{}'   expected: '{}'", data, expected);
+	@ParameterizedTest
+	@CsvSource({ "complement(CP001663.1:6463934..6465826),CP001663.1",
+			"complement(NC_000932.1:69611..69724),NC_000932.1" })
+	public void extractAccessionTest(String data, String expected) throws Exception {
 
 		InsdcParser parser = new InsdcParser(DataSource.GENBANK);
 		Location loc = parser.parse(data);
@@ -74,29 +65,25 @@ public class InsdcParserTest {
 		if (!loc.isComplex()) {
 			log.info("simple location: {}", data);
 			log.debug("\taccession: '{}'  expected: '{}'", loc.getAccession().getID(), expected);
-			Assert.assertEquals(expected, loc.getAccession().getID());
+			assertEquals(expected, loc.getAccession().getID());
 		}
 	}
 
 	@Test
-	public void testParser(){
-	String[] testStrings = {
-		"J00194.1:100..202",
-		"A00001.5:34..45",
-		"43..129",
-		"bond(55,110)",
-		"bond(34,35),join(56..80),complement(45,73)",
-		"order(complement(30,40),70..80),bond(34,35),join(56,80),complement(45..56)",
-		"join(join(complement(30,40),complement(70..80)),bond(34,35),join(56,80),complement(45..56))",
-		"complement(join(complement(2000..4000),complement(70..80)),bond(34,35),join(56,80),complement(45..56))",
+	public void testParser() {
+		String[] testStrings = { "J00194.1:100..202", "A00001.5:34..45", "43..129", "bond(55,110)",
+				"bond(34,35),join(56..80),complement(45,73)",
+				"order(complement(30,40),70..80),bond(34,35),join(56,80),complement(45..56)",
+				"join(join(complement(30,40),complement(70..80)),bond(34,35),join(56,80),complement(45..56))",
+				"complement(join(complement(2000..4000),complement(70..80)),bond(34,35),join(56,80),complement(45..56))",
 
-	};
-	InsdcParser p = new InsdcParser();
-	p.setComplexFeaturesAppendMode(complexFeaturesAppendEnum.HIERARCHICAL);
+		};
+		InsdcParser p = new InsdcParser();
+		p.setComplexFeaturesAppendMode(complexFeaturesAppendEnum.HIERARCHICAL);
 
-	for (String s: testStrings){
-		Location l = p.parse(s);
-		assertNotNull(l);
+		for (String s : testStrings) {
+			Location l = p.parse(s);
+			assertNotNull(l);
+		}
 	}
-}
 }
