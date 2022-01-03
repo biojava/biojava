c1a114fa5c8be7b616e7e7507d7bbe443f5d0b54
richard <ra22597@gmail.com>
2022-01-03 12:24:48 +0000

    simplify generics in RNASequence

---
+++ biojava-core/src/test/java/org/biojava/nbio/core/sequence/RNASequenceTest.java
@@ -0,0 +1,7 @@
+package org.biojava.nbio.core.sequence;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RNASequenceTest {
+
+}
--- biojava-core/src/test/java/org/biojava/nbio/core/sequence/TaxonomyIDTest.java
+++ biojava-core/src/test/java/org/biojava/nbio/core/sequence/TaxonomyIDTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaxonomyIDTest {
-    
+
     @Test
     void createTaxonomyID(){
         TaxonomyID tId = new TaxonomyID("abc1", DataSource.GENBANK);
