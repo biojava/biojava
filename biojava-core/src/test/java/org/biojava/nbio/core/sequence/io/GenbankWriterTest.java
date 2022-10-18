7f116c9a87c95b82237b3286d51c2bb73e6288c6
jamesmorris <kslb056@UKCMXL308504186.astrazeneca.net>
2022-10-18 15:08:13 +0100

    removed unused import

--- biojava-core/src/test/java/org/biojava/nbio/core/sequence/io/GenbankWriterTest.java
+++ biojava-core/src/test/java/org/biojava/nbio/core/sequence/io/GenbankWriterTest.java
@@ -46,7 +46,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
